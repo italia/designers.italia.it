69a2b66531aa3c8d579e144a1be913eb3328f647
Danilo Spinelli <danilo.spinelli@agid.gov.it>
2017-05-24 14:58:09 +0200

    plane shaded

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -3,7 +3,6 @@ $(function() {
 
     $('*[data-grid-accordion]').on('click', function(e) {
         var toShow = $(this).data('gridAccordion');
-        $gridcontainer.attr('class','section__designkit__grid');
         $(this).toggleClass('active');
         $gridcontainer.toggleClass(toShow);
     });
