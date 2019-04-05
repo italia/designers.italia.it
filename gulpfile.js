57aa36145a6f4b6e9309de536faaf3baea479fbb
Francesco Zaia <francesco.zaia@gmail.com>
2019-04-05 16:20:52 +0200

    Aggiornamento dipendenze (#151)
    * Aggiornate dipendenze Ruby e NodeJS
    - Rimosse gems "minima", "hawkins"
    - Aggiornato a "gulp@4.0.0", rimosso packages "imagemin-*" perché inclusi nel nuovo "gulp-imagemin@5.0.3"
    
    * Aggiornamento file config per CircleCI
    
    * Utilizzo di Yarn al posto di NPM, aggiornato Readme

--- gulpfile.js
+++ gulpfile.js
@@ -1,58 +1,54 @@
 var gulp = require('gulp');
-var gutil = require('gulp-util');
+var log = require('fancy-log');
 var fs = require('fs');
 var os = require('os');
 var path = require('path');
-var configFile = path.join(os.homedir(), ".designersitalia_site.json");
+var configFile = path.join(os.homedir(), '.designersitalia_site.json');
 
 var config = {};
 if (fs.existsSync(configFile)) {
-  gutil.log("Reading local config from [" + configFile + "]");
+  log('Reading local config from [' + configFile + ']');
   config = require(configFile);
 } else {
-  gutil.log("No local config found at [" + configFile + "]");
+  log('No local config found at [' + configFile + ']');
 }
 
 var runSequence = require('run-sequence');
 var clean = require('gulp-clean');
 var shell = require('gulp-shell');
 var imagemin = require('gulp-imagemin');
-var pngquant = require('imagemin-pngquant');
-var jpegtran = require('imagemin-jpegtran');
-var gifsicle = require('imagemin-gifsicle');
 var minifyHTML = require('gulp-htmlmin');
-var autoprefixer = require('gulp-autoprefixer');
-var uncss = require('gulp-uncss');
-var cleanCss = require('gulp-clean-css');
-var concat = require('gulp-concat');
-var rename = require('gulp-rename');
-var uglify = require('gulp-uglify');
 var rsync = require('gulp-rsync');
 
-gulp.task('jekyll', function() {
+gulp.task('jekyll', function () {
   return gulp.src('README.md', {
-      read: false
-    })
+    read: false
+  })
     .pipe(shell([
       'JEKYLL_ENV=production bundle exec jekyll build'
     ]));
 });
 
-gulp.task('optimize-images', function() {
-  return gulp.src(['_site/**/*.jpg', '_site/**/*.jpeg', '_site/**/*.gif',
-      '_site/**/*.png'
-    ])
-    .pipe(imagemin({
-      progressive: false,
-      svgoPlugins: [{
-        removeViewBox: false
-      }],
-      use: [pngquant(), jpegtran(), gifsicle()]
-    }))
-    .pipe(gulp.dest('_site/'));
+gulp.task('optimize-images', function () {
+  return gulp.src([
+    '_site/**/*.jpg',
+    '_site/**/*.jpeg',
+    '_site/**/*.gif',
+    '_site/**/*.png'
+  ]).pipe(imagemin([
+    imagemin.gifsicle({interlaced: true}),
+    imagemin.jpegtran({progressive: true}),
+    imagemin.optipng({optimizationLevel: 5}),
+    imagemin.svgo({
+      plugins: [
+        {removeViewBox: false},
+        {cleanupIDs: false}
+      ]
+    })
+  ])).pipe(gulp.dest('_site/'));
 });
 
-gulp.task('optimize-html', function() {
+gulp.task('optimize-html', function () {
   return gulp.src('_site/**/*.html')
     .pipe(minifyHTML({
       quotes: true
@@ -60,48 +56,25 @@ gulp.task('optimize-html', function() {
     .pipe(gulp.dest('_site/'));
 });
 
-gulp.task('optimize-css', function() {
-  return gulp.src('styles/*.css')
-    // .pipe(autoprefixer())
-    // .pipe(uncss({
-    //   html: ['_site/**/*.html'],
-    //   ignore: []
-    // }))
-    .pipe(cleanCss({
-      keepBreaks: false
-    }))
-    .pipe(gulp.dest('_site/styles'));
-});
-
-gulp.task('optimize-js', function() {
-  return gulp.src("js/*.js")
-    .pipe(concat('scripts.js'))
-    // .pipe(gulp.dest("_site/js"))
-    .pipe(rename('scripts.min.js'))
-    .pipe(uglify())
-    .pipe(gulp.dest("_site/js"));
-});
-
-gulp.task('clean', function() {
+gulp.task('clean', function () {
   return gulp.src('_site', {
-      read: false
-    })
+    read: false,
+    allowEmpty: true
+  })
     .pipe(clean());
 });
 
-gulp.task('build', function(cb) {
-  runSequence(
-    'clean',
-    'jekyll', [
-      // 'optimize-js',
-      // 'optimize-css',
-      'optimize-html'
-      //'optimize-images'
-    ], cb);
-});
+
+gulp.task('optimize', gulp.parallel('optimize-html', 'optimize-images', function optimize(done) {
+  done();
+}));
+
+gulp.task('build', gulp.series('clean', 'jekyll', 'optimize', function build(done) {
+  done();
+}));
 
 if (config.staging || process.env.DESIGNERSITALIA_SITE_STAGING_SERVER) {
-  gulp.task('publish-staging', function() {
+  gulp.task('publish-staging', function () {
     var publish_server = process.env.DESIGNERSITALIA_SITE_STAGING_SERVER || config.staging.server;
     var publish_destination = process.env.DESIGNERSITALIA_SITE_STAGING_PATH || config.staging.path;
     var publish_port = process.env.DESIGNERSITALIA_SITE_STAGING_PORT || config.staging.port;
@@ -122,11 +95,11 @@ if (config.staging || process.env.DESIGNERSITALIA_SITE_STAGING_SERVER) {
       }));
   });
 } else {
-  gutil.log("No config for staging publish, task will be disabled");
+  log('No config for staging publish, task will be disabled');
 }
 
 if (config.production || process.env.DESIGNERSITALIA_SITE_PRODUCTION_SERVER) {
-  gulp.task('publish-production', function() {
+  gulp.task('publish-production', function () {
     var publish_server = process.env.DESIGNERSITALIA_SITE_PRODUCTION_SERVER || config.production.server;
     var publish_destination = process.env.DESIGNERSITALIA_SITE_PRODUCTION_PATH || config.production.path;
     var publish_port = process.env.DESIGNERSITALIA_SITE_PRODUCTION_PORT || config.production.port;
@@ -144,5 +117,5 @@ if (config.production || process.env.DESIGNERSITALIA_SITE_PRODUCTION_SERVER) {
       }));
   });
 } else {
-  gutil.log("No config for production publish, task will be disabled");
+  log('No config for production publish, task will be disabled');
 }
