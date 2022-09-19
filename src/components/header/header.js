c7ce6c264a15ebf58d98a5564484a01a124ee562
Gabriele Peirolo <g.peirolo@wedoo.it>
2022-09-19 11:12:34 +0200

    feat: basic header and footer components

---
+++ src/components/header/header.js
@@ -0,0 +1,12 @@
+import React from "react"
+import "./header.scss"
+
+const Header = ({ props }) => {
+  return (
+    <header className="bg-primary">
+      <p className="text-white">Header</p>
+    </header>
+  )
+}
+
+export default Header
--- src/pages/home.js
+++ src/pages/home.js
@@ -5,7 +5,8 @@ import {Seo} from "../components/seo"
 const HomePage = () => {
 	return (
 	  <Template>
-			
+      <h1>Page content</h1>
+      <p>Maecenas vel efficitur felis. In elementum scelerisque molestie. Integer ac ex quis erat vestibulum vestibulum ut vitae erat. Ut enim ipsum, euismod luctus nunc sit amet, elementum convallis enim. Nunc turpis dolor, aliquam sed metus in, aliquet dignissim augue. Mauris eros massa, blandit eget tortor ac, ornare pellentesque ante. Ut pretium accumsan elit, at ultrices nisl. Quisque pulvinar nibh nisi, id elementum sem faucibus nec. Vivamus commodo placerat elit, ac dignissim nibh sodales ac. Pellentesque consectetur nisi erat, vel dignissim elit eleifend feugiat. Pellentesque enim eros, convallis id ante quis, iaculis vulputate enim.</p>
 	  </Template>
 	)
  }
