42b834f592d6bc3c62c4b373eb44818851304e74
Gabriele Peirolo <g.peirolo@wedoo.it>
2022-09-19 17:24:08 +0200

    feat: section component

---
+++ src/components/section/section.js
@@ -0,0 +1,23 @@
+import React from "react"
+
+const Section = ({bgColor, bgImage, whiteText, describedBy, children}) => {
+  let sectionnStyles = 'section'
+		+ `${bgColor ? ' section-'+bgColor : ''}`   // muted, primary, neutral
+		+ `${bgImage ? ' section-image' : ''}`      // url
+    + `${whiteText ? ' white-color' : ''}`      // boolean for white text
+
+  return (
+
+    <section
+      className={sectionnStyles}
+      aria-describedby={describedBy}
+      style={{backgroundImage: `url(${bgImage})`}}
+    >
+      <div className="section-content">
+        {children}
+      </div>
+    </section>
+  )
+}
+
+export default Section
--- src/pages/home.js
+++ src/pages/home.js
@@ -3,45 +3,72 @@ import Template from "../templates/default"
 import {Seo} from "../components/seo/seo"
 import Icon from "../components/icon/icon"
 import Button from "../components/button/button"
+import Section from "../components/section/section"
 
 const HomePage = () => {
 	return (
 	  <Template>
-      <h1>Page content</h1>
-      <p>Maecenas vel efficitur felis. In elementum scelerisque molestie. Integer ac ex quis erat vestibulum vestibulum ut vitae erat. Ut enim ipsum, euismod luctus nunc sit amet, elementum convallis enim. Nunc turpis dolor, aliquam sed metus in, aliquet dignissim augue. Mauris eros massa, blandit eget tortor ac, ornare pellentesque ante. Ut pretium accumsan elit, at ultrices nisl. Quisque pulvinar nibh nisi, id elementum sem faucibus nec. Vivamus commodo placerat elit, ac dignissim nibh sodales ac. Pellentesque consectetur nisi erat, vel dignissim elit eleifend feugiat. Pellentesque enim eros, convallis id ante quis, iaculis vulputate enim.</p>
-		<Icon icon="sprites.svg#it-tool" size="lg" bg="light" color="success"></Icon>
-		<Button
-			style="primary"
-			iconLeft={
-				<Icon icon="sprites.svg#it-tool" size="sm" color="light"/>
-			}>
-			Label del bottone con icona a sinistra
-		</Button>
-		<Button
-			style="primary"
-			iconRight={
-				<Icon icon="sprites.svg#it-tool" size="sm" color="light"/>
-			}>
-			Label del bottone con icona a destra
-		</Button>
-		<Button
-			style="primary"
-			size="lg"
-			iconRounded
-			iconLeft={
-				<Icon icon="sprites.svg#it-tool" size="sm" color="primary"/>
-			}>
-			Label del bottone con icona stondata a sinistra
-		</Button>
-		<Button
-			style="primary"
-			iconRounded
-			srOnly="Testo per screen reader"
-			iconLeft={
-				<Icon icon="sprites.svg#it-tool" size="sm" color="primary"/>
-			}>
-		</Button>
-		<Button role="button" url="#" style="secondary">Label del link</Button>
+      <Section>
+        <h1>Page content</h1>
+        <p>Maecenas vel efficitur felis. In elementum scelerisque molestie. Integer ac ex quis erat vestibulum vestibulum ut vitae erat. Ut enim ipsum, euismod luctus nunc sit amet, elementum convallis enim. Nunc turpis dolor, aliquam sed metus in, aliquet dignissim augue. Mauris eros massa, blandit eget tortor ac, ornare pellentesque ante. Ut pretium accumsan elit, at ultrices nisl. Quisque pulvinar nibh nisi, id elementum sem faucibus nec. Vivamus commodo placerat elit, ac dignissim nibh sodales ac. Pellentesque consectetur nisi erat, vel dignissim elit eleifend feugiat. Pellentesque enim eros, convallis id ante quis, iaculis vulputate enim.</p>
+      </Section>
+      <Section
+        bgColor="muted"
+      >
+        <h2>Section muted</h2>
+        <Icon icon="sprites.svg#it-tool" size="lg" bg="light" color="success"></Icon>
+        <Button
+          btnStyle="primary"
+          iconLeft={
+            <Icon icon="sprites.svg#it-tool" size="sm" color="light"/>
+          }>
+          Label del bottone con icona a sinistra
+        </Button>
+        <Button
+          btnStyle="primary"
+          iconRight={
+            <Icon icon="sprites.svg#it-tool" size="sm" color="light"/>
+          }>
+          Label del bottone con icona a destra
+        </Button>
+        <Button
+          btnStyle="primary"
+          size="lg"
+          iconRounded
+          iconLeft={
+            <Icon icon="sprites.svg#it-tool" size="sm" color="primary"/>
+          }>
+          Label del bottone con icona stondata a sinistra
+        </Button>
+        <Button
+          btnStyle="primary"
+          iconRounded
+          srOnly="Testo per screen reader"
+          iconLeft={
+            <Icon icon="sprites.svg#it-tool" size="sm" color="primary"/>
+          }>
+        </Button>
+        <Button
+          role="button"
+          url="#"
+          btnStyle="secondary">
+            Label del link
+        </Button>
+      </Section>
+      <Section
+        bgColor="neutral"
+        whiteText={true}
+      >
+        <h2>Section neutral</h2>
+        <p>Maecenas vel efficitur felis. In elementum scelerisque molestie. Integer ac ex quis erat vestibulum vestibulum ut vitae erat. Ut enim ipsum, euismod luctus nunc sit amet, elementum convallis enim. Nunc turpis dolor, aliquam sed metus in, aliquet dignissim augue. </p>
+      </Section>
+      <Section
+        bgImage="https://picsum.photos/1280/720?image=811"
+        whiteText={true}
+      >
+        <h2>Section with background image</h2>
+        <p>Maecenas vel efficitur felis. In elementum scelerisque molestie. Integer ac ex quis erat vestibulum vestibulum ut vitae erat. Ut enim ipsum, euismod luctus nunc sit amet, elementum convallis enim. Nunc turpis dolor, aliquam sed metus in, aliquet dignissim augue. </p>
+      </Section>
 	  </Template>
 	)
  }
