6b03e6380c51b6f27892b0fc4107cd20d81dc65f
Oscar Bresolin <o.bresolin@wedoo.it>
2022-09-19 11:14:08 +0200

    fix: image path

--- src/components/seo.js
+++ src/components/seo.js
@@ -50,15 +50,15 @@ export const Seo = ({title, description, image, pathname, children}) => {
 			<meta name="twitter:image" content={seo.twitter_image} />
 			<meta name="twitter:site" content={seo.twitter_site} />
 			<meta name="twitter:creator" content={seo.twitter_creator} />
-			<link rel="apple-touch-icon" sizes="180x180" href="images/favicons/apple-touch-icon.png"/>
-			<link rel="icon" type="image/png" href="images/favicons/favicon-32x32.png" sizes="32x32"/>
-			<link rel="icon" type="image/png" href="images/favicons/favicon-16x16.png" sizes="16x16"/>
-			<link rel="manifest" href="images/favicons/manifest.json"/>
-			<link rel="mask-icon" href="images/favicons/safari-pinned-tab.svg"/>
-			<link rel="shortcut icon" href="images/favicons/favicon.ico"/>
+			<link rel="apple-touch-icon" sizes="180x180" href="../images/favicons/apple-touch-icon.png"/>
+			<link rel="icon" type="image/png" href="../images/favicons/favicon-32x32.png" sizes="32x32"/>
+			<link rel="icon" type="image/png" href="../images/favicons/favicon-16x16.png" sizes="16x16"/>
+			<link rel="manifest" href="../images/favicons/manifest.json"/>
+			<link rel="mask-icon" href="../images/favicons/safari-pinned-tab.svg"/>
+			<link rel="shortcut icon" href="../images/favicons/favicon.ico"/>
 			<meta name="apple-mobile-web-app-title" content="Designers Italia"/>
 			<meta name="application-name" content="Designers Italia"/>
-			<meta name="msapplication-config" content="images/favicons/browserconfig.xml"/>
+			<meta name="msapplication-config" content="../images/favicons/browserconfig.xml"/>
 			<meta name="theme-color" content={seo.theme_color}/>
 			{children}
 		</>
