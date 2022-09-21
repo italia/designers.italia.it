bddd69b61e949335b8812b4e70638497489b3448
Oscar Bresolin <o.bresolin@wedoo.it>
2022-09-21 11:51:47 +0200

    fix: clean

--- src/pages/yaml.js
+++ src/pages/yaml.js
@@ -5,19 +5,10 @@ import TestYaml from "../components/test-yaml/test-yaml"
 import Data from '../data/pages/home.yaml'
 
 const Yaml = ({data}) => {
-	var Handler
 	return (
 	  <Template>
 			
-			{Data.components.map((value,index)=>{
-				Handler = "TestYaml"
-				return (
-					<Handler></Handler>
-					
-				)
-			})}
 	  </Template>
-			
 	)
 }
 
