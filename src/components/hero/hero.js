a63fe58dd6a9a5817c82fcb61faae1b484439dd8
Oscar Bresolin <oscareb@gmail.com>
2022-10-10 11:11:28 +0200

    feat: hero top

--- src/components/hero/hero.js
+++ src/components/hero/hero.js
@@ -1,29 +1,40 @@
 import * as React from "react"
 import './hero.scss'
 import ReactMarkdown from 'react-markdown'
+import Breadcrumbs from "../breadcrumbs/breadcrumbs"
+import Dropdown from "../dropdown/dropdown"
 
 const Hero =({
 	breadcrumbs,
-	share,			
-	tag,			
+	share,
+	tag,
 	background,
-	title,			
-	subtitle,		
-	pretext,		
-	text,			
-	img,			
-	bgImg,	
+	title,
+	subtitle,
+	pretext,
+	text,
+	img,
+	bgImg,
 	Kangaroo
 
 })=>{
+  let styles = 'hero'
+  + `${background ? ' '+background : ''}`
 	return(
-		<div className="hero">
-			<div className="hero-top">
+		<div className={styles}>
+      <div className="container">
+        <div className="row">
+          <div className="col-12">
+            <div className="hero-top px-3 px-md-0 px-lg-3 py-3">
+              <Breadcrumbs {...breadcrumbs}></Breadcrumbs>
+              {share  && <Dropdown {...share}></Dropdown>}
+            </div>
+            <div className="hero-main">
 
-			</div>
-			<div className="hero-main">
-				
-			</div>
+            </div>
+          </div>
+        </div>
+      </div>
 		</div>
 	)
 }
