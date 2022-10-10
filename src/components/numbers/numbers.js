f4a3736d92342cc18b9397b303cf8a2bac9d4d0d
Oscar Bresolin <oscareb@gmail.com>
2022-10-10 09:51:44 +0200

    fix: label inversion highlight

--- src/components/numbers/numbers.js
+++ src/components/numbers/numbers.js
@@ -10,13 +10,13 @@ const Numbers = ({props}) => {
 		NumbersRender = props.map((num,index) => {
 			return(
         <div className="numbers d-flex justify-content-start align-content-start mb-3 mb-md-4 me-5" key={"number-"+index}>
+          <div className="label"><small><strong>{num.label}</strong></small></div>
           <Icon
             icon={num.icon}
             size="lg"
-            className="me-2"
+            addonClasses="ms-4"
           />
-          <div className="number font-monospace fw-normal h2 me-4 mb-0">{num.number}</div>
-          <div className="label"><small><strong>{num.label}</strong></small></div>
+          <div className="number font-monospace fw-normal h2 mb-0">{num.number}</div>
         </div>
 			)
 		})
