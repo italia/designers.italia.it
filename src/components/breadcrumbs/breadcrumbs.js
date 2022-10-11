2ad44d6e8c1e48562f5b988cbd29245b4869425e
Gabriele Peirolo <g.peirolo@wedoo.it>
2022-10-11 09:48:30 +0200

    fix: breadcrumbs null state

--- src/components/breadcrumbs/breadcrumbs.js
+++ src/components/breadcrumbs/breadcrumbs.js
@@ -11,22 +11,27 @@ const Breadcrumbs = ({
 	let breadcrumbsStyles = 'breadcrumb'
 		+ `${dark ? ' dark px-3' : ''}`
 
+  let breadcrumbItems = null
+
+  if (items) {
+    breadcrumbItems = items.map((item,index) => {
+      let ariaCurrent = item.active ? 'page' : undefined
+      return(
+        <li key={"bread-"+index} className={`breadcrumb-item ${item.active ? 'active' : ''}`} aria-current={ariaCurrent}>
+          { item.icon ? <Icon icon={item.icon} size="sm" color="light" addonClasses="align-top me-1"/> : null }
+          <a href={item.url}>{item.label}</a>
+          {!item.active && <span className="separator">{separator}</span>}
+        </li>
+      )
+    })
+  }
 
 	return (
 		<nav className="breadcrumb-container" aria-label={ariaLabel}>
 			<ol
 				className={breadcrumbsStyles}
 			>
-        {items.map((item,index) => {
-          let ariaCurrent = item.active ? 'page' : undefined
-          return(
-            <li key={"bread-"+index} className={`breadcrumb-item ${item.active ? 'active' : ''}`} aria-current={ariaCurrent}>
-              { item.icon ? <Icon icon={item.icon} size="sm" color="light" addonClasses="align-top me-1"/> : null }
-              <a href={item.url}>{item.label}</a>
-              {!item.active && <span className="separator">{separator}</span>}
-            </li>
-          )
-        })}
+        {breadcrumbItems}
 			</ol>
 		</nav>
 	)
