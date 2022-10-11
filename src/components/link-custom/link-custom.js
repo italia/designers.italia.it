48f12519343a3d40929fbec2586a6850b7d8b1ad
Gabriele Peirolo <g.peirolo@wedoo.it>
2022-10-11 12:04:56 +0200

    feat: navOtherPrevNext component

--- src/components/link-custom/link-custom.js
+++ src/components/link-custom/link-custom.js
@@ -1,19 +1,26 @@
 import React from "react"
+import Icon from "../icon/icon"
+import "./link-custom.scss"
 
 const LinkCustom = ({
   label,
   url,
   image,
-  blank
+  blank,
+  specular,
+  icon
 }) => {
+  let linkStyles = "link-custom d-inline-flex align-items-center text-decoration-none"
+    + `${specular ? ' flex-row-reverse text-end' : ''}`
   return (
     <a
-      className="link-custom d-inline-flex align-items-center text-decoration-none"
+      className={linkStyles}
       href={url}
       target={blank ? "_blank" : undefined}
     >
-      <img src={image} aria-hidden="true" className="me-2"/>
-      <small><strong>{label}</strong></small>
+      <Icon {...icon}/>
+      <img src={image} aria-hidden="true" className="mx-3 flex-shrink-0"/>
+      <strong>{label}</strong>
     </a>
   )
 }
---
+++ src/components/nav-other-prev-next/nav-other-prev-next.js
@@ -0,0 +1,39 @@
+import React from "react"
+import Icon from "../icon/icon"
+import LinkCustom from "../link-custom/link-custom"
+
+const NavOtherPrevNext = ({
+  prev,
+  next
+}) => {
+  let iconPrev = ({
+    icon: "sprites.svg#it-arrow-left",
+    size: "sm",
+    align : "middle",
+    color :"primary",
+    hidden: true,
+  })
+  let iconNext = {
+    icon: "sprites.svg#it-arrow-right",
+    size: "sm",
+    align : "middle",
+    color :"primary",
+    hidden: true,
+  }
+  return (
+    <div className="nav-other-prev-next border neutral-1-border-color-a3 py-5 border-end-0 border-start-0">
+      <div className="container-xxl">
+        <div className="row">
+          <div className="col mb-4 mb-md-0 col-md-5 col-lg-4 col-xl-3">
+            <LinkCustom {...prev}/>
+          </div>
+          <div className="col col-md-5 offset-md-2 col-lg-4 offset-lg-4 col-xl-3 offset-xl-6">
+            <LinkCustom {...next}/>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default NavOtherPrevNext
