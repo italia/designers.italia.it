262385409d0ec2c2ff4ef473d01cfb13dbfc380e
Gabriele Peirolo <g.peirolo@wedoo.it>
2022-11-14 17:49:06 +0100

    chore: resourcelist refactor

--- src/components/resource-list/resource-list.js
+++ src/components/resource-list/resource-list.js
@@ -16,18 +16,34 @@ const ResourceList = ({
 		HLevel = `h3`
 	}
 
-  let addonClasses = full ? ' px-0 mx-0 w-100' : 'col col-md-10 offset-md-1 col-lg-9'
+  let addonClasses = full ? ' px-0 mx-0 w-100 pb-5' : 'col-12 col-md-10 offset-md-1 col-lg-9 offset-lg-0 pb-5'
+  let listItem
+
+  if (list) {
+    listItem = list.map((item,index) => {
+      return(
+        <>
+          <div className={addonClasses}>
+              <div className="px-3 px-lg-0">
+                <List {...item} key={"list-"+index}/>
+              </div>
+          </div>
+
+        </>
+      )
+    })
+  }
 
   return (
     <div className="resource-list py-5">
       <div className="container-xxl">
-        <div className="row">
+        <div className="row justify-content-lg-center">
           <div className={addonClasses}>
               <div className="px-3 px-lg-0">
-                {title && <HLevel className="mb-4">{title}</HLevel>}
-                <List {...list}/>
+                {title && <HLevel className="mb-0">{title}</HLevel>}
               </div>
           </div>
+          {listItem}
         </div>
       </div>
     </div>
