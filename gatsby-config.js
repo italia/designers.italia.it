f8d3144f8fd904e8db735cc319f481ff5f278e9d
Gabriele Peirolo <g.peirolo@wedoo.it>
2022-11-15 09:19:47 +0100

    chore: commented out htaccess plugin - for prod only

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,7 +17,7 @@ module.exports = {
     `gatsby-plugin-image`,
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`, // Needed for dynamic images
-    {
+    /*{
       resolve: 'gatsby-plugin-htaccess',
       // docs here: https://www.npmjs.com/package/gatsby-plugin-htaccess
       options: {
@@ -25,22 +25,22 @@ module.exports = {
         https: true,
         www: true,
         SymLinksIfOwnerMatch: true,
-        host: 'designers.italia.it/', // if 'www' is set to 'false', be sure to also remove it here!
-        /*ErrorDocument: `
+        host: 'designers.italia.it', // if 'www' is set to 'false', be sure to also remove it here!
+        ErrorDocument: `
           ErrorDocument 401 /error_pages/401.html
           ErrorDocument 404 /error_pages/404.html
           ErrorDocument 500 /error_pages/500.html
-        `,*/
+        `,
         redirect: [
           // redirects go here
           'RewriteRule ^not-existing-url/?$ /existing-url [R=301,L,NE]',
         ],
-        /*custom: `
+        custom: `
             # This is a custom rule!
             # This is a another custom rule!
-        `,*/
+        `,
       },
-    },
+    },*/
   ],
   pathPrefix: '/',
   flags: {
