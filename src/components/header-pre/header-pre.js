264833cdc1ba897ad6bdb0a69b2fcdb331d56925
fupete <hereisfupete@gmail.com>
2022-11-26 11:23:14 +0100

    hide pre header on sticky

--- src/components/header-pre/header-pre.js
+++ src/components/header-pre/header-pre.js
@@ -3,12 +3,15 @@ import React from "react"
 const HeaderPre = ({data}) => {
 
 	return (
-		<div className="header-pre bg-light py-2 small">
-			<div className="container-xxl">
-				<div className="row">
-					<div className="col-12">
-						<div className="text-center">
-						  <span className="fw-semibold">{data.title}</span> - <a href={data.url}>{data.link}</a>
+		// XXX remove first wrapper div to view on sticky
+		<div className="it-header-slim-wrapper">   
+			<div className="header-pre bg-light py-2 small">
+				<div className="container-xxl">
+					<div className="row">
+						<div className="col-12">
+							<div className="text-center">
+							<span className="fw-semibold">{data.title}</span> - <a href={data.url}>{data.link}</a>
+							</div>
 						</div>
 					</div>
 				</div>
