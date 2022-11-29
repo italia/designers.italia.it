28d04740a8feddd9b63fb27cfa090be0369681cd
Massimo Marsan <m.marsan@wedoo.it>
2022-11-29 13:48:17 +0100

    fix: mouse focus script in build
    issue #477

--- src/js/globals.js
+++ src/js/globals.js
@@ -1,3 +1,8 @@
+import { TrackFocus } from "bootstrap-italia/dist/bootstrap-italia.esm"
+import { FontsLoader } from "bootstrap-italia/dist/bootstrap-italia.esm"
+
+const trackFocusDummy = TrackFocus //trick treeshaking
+
 if (typeof document !== "undefined") {
   window.__PUBLIC_PATH__ = '/fonts'
 }
