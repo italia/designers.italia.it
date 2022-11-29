452d4d67dc61cdafe72f8384fe30694b9c872117
fupete <hereisfupete@gmail.com>
2022-11-29 08:54:34 +0100

    fix/pathname

--- src/pages/risorse-per-progettare/realizzare/sviluppo-interfaccia.js
+++ src/pages/risorse-per-progettare/realizzare/sviluppo-interfaccia.js
@@ -1,6 +1,6 @@
 import * as React from "react"
-import Template from "../../../../templates/tmpl-level-4"
-import {Seo} from "../../../../components/seo/seo"
+import Template from "../../../templates/tmpl-level-3"
+import {Seo} from "../../../components/seo/seo"
 
 import Pagedata from "./sviluppo-interfaccia.yaml"
 
