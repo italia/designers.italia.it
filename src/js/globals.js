3a92d34d836c97fba70c054095b0a255ae8c8154
Massimo Marsan <m.marsan@wedoo.it>
2022-11-29 16:01:55 +0100

    fix: moved script init to avoid fonts loader conflict

--- src/js/globals.js
+++ src/js/globals.js
@@ -1,8 +1,3 @@
-import { TrackFocus } from "bootstrap-italia/dist/bootstrap-italia.esm"
-import { FontsLoader } from "bootstrap-italia/dist/bootstrap-italia.esm"
-
-const trackFocusDummy = TrackFocus //trick treeshaking
-
 if (typeof document !== "undefined") {
   window.__PUBLIC_PATH__ = '/fonts'
 }
