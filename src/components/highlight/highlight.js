a7c9c0a26d5e5f183759d4a547b5d27e99f382b7
Oscar Bresolin <oscareb@gmail.com>
2022-11-30 15:00:25 +0100

    fix: highlight update

--- src/components/highlight/highlight.js
+++ src/components/highlight/highlight.js
@@ -62,7 +62,7 @@ const Highlight = (
 
   let ratioClass
   if (fullImg) {
-    ratioClass = "img-container"
+    ratioClass = "img-container full"
   }else{
     ratioClass = "img-container ratio ratio-16x9"
   }
