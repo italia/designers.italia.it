fe7188094190ae5e5b59961db1dd62da3ac3378d
Daniele T <Fupete@users.noreply.github.com>
2022-11-30 12:33:06 +0100

    Feat/next content comprendere (#455)
    * feat, fixes and contents for section "Comprendere"
    
    Co-authored-by: Daniele T <Fupete@users.noreply.github.com>
    Co-authored-by: fupete <hereisfupete@gmail.com>
    
    Co-authored-by: Gianvito Fanelli <30351061+gianvitofanelli@users.noreply.github.com>

--- src/pages/risorse-per-progettare/comprendere/esperienza-utente.js
+++ src/pages/risorse-per-progettare/comprendere/esperienza-utente.js
@@ -1,10 +1,10 @@
 import * as React from "react"
-import Template from "../../templates/tmpl-level-2"
-import {Seo} from "../../components/seo/seo"
+import Template from "../../../templates/tmpl-level-3"
+import {Seo} from "../../../components/seo/seo"
 
-import Pagedata from "./level2.yaml"
+import Pagedata from "./esperienza-utente.yaml"
 
-const Level2 = () => {
+const Level3 = () => {
   return(
     <Template Pagedata={Pagedata}>
       {/* place extra components / HTML here */}
@@ -12,7 +12,7 @@ const Level2 = () => {
   )
 }
 
-export default Level2
+export default Level3
 
 export const Head = () => (
 	<Seo
