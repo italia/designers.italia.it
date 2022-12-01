47e99dc01832b9c4e321efd6c49c861a32681716
fupete <hereisfupete@gmail.com>
2022-12-01 15:48:58 +0100

    feat/updated template and fixes from next

--- src/pages/modelli/comuni/adotta-il-modello-di-sito-comunale/comprendi-lo-stato-dell-arte.js
+++ src/pages/modelli/comuni/adotta-il-modello-di-sito-comunale/comprendi-lo-stato-dell-arte.js
@@ -4,9 +4,9 @@ import {Seo} from "../../../../components/seo/seo"
 
 import Pagedata from "./comprendi-lo-stato-dell-arte.yaml"
 
-const Level4 = () => {
+const Level4 = ({ pageContext }) => {
   return(
-    <Template Pagedata={Pagedata}>
+    <Template Pagedata={Pagedata} pageContext={pageContext}>
       {/* place extra components / HTML here */}
     </Template>
   )
