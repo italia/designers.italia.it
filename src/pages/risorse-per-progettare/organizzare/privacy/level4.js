01e0ba738b101a171449fb65d6120e7dd1895479
fupete <hereisfupete@gmail.com>
2022-12-01 07:54:45 +0100

    fix/passing location through pages

--- src/pages/risorse-per-progettare/organizzare/privacy/level4.js
+++ src/pages/risorse-per-progettare/organizzare/privacy/level4.js
@@ -4,9 +4,9 @@ import {Seo} from "../../../../components/seo/seo"
 
 import Pagedata from "./level4.yaml"
 
-const Level4 = ({ pageContext }) => {  	
+const Level4 = ({ pageContext,location }) => {  	
   return(
-    <Template Pagedata={Pagedata} pageContext={pageContext}>
+    <Template Pagedata={Pagedata} pageContext={pageContext} location={location}>
       {/* place extra components / HTML here */}
     </Template>
   )
