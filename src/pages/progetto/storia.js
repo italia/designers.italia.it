b5ea4accf4a9fae8130525fd06ac278d786d7f59
fupete <hereisfupete@gmail.com>
2022-12-02 12:20:00 +0100

    fix/merge typo

--- src/pages/progetto/storia.js
+++ src/pages/progetto/storia.js
@@ -2,11 +2,7 @@ import * as React from "react"
 import Template from "../../templates/tmpl-level-2"
 import {Seo} from "../../components/seo/seo"
 
-<<<<<<<< HEAD:src/pages/risorse-per-progettare/organizzare.js
-import Pagedata from "./organizzare.yaml"
-========
 import Pagedata from "./storia.yaml"
->>>>>>>> next:src/pages/progetto/storia.js
 
 const Level2 = ({ pageContext }) => {
   return(
