b9df021bc5eaed408fcfae41cd2443532f811c9c
fupete <hereisfupete@gmail.com>
2022-12-02 16:45:29 +0100

    feat/pageContext in modelli

--- src/pages/modelli/comuni/adotta-il-modello-di-servizi-digitali-comunali/progetta-il-flusso-di-servizio.js
+++ src/pages/modelli/comuni/adotta-il-modello-di-servizi-digitali-comunali/progetta-il-flusso-di-servizio.js
@@ -4,9 +4,9 @@ import {Seo} from "../../../../components/seo/seo"
 
 import Pagedata from "./progetta-il-flusso-di-servizio.yaml"
 
-const Level4 = ({ pageContext }) => {
+const Level4 = ({ pageContext, location }) => {
   return(
-    <Template Pagedata={Pagedata} pageContext={pageContext}>
+    <Template Pagedata={Pagedata} pageContext={pageContext} location={location}>
       {/* place extra components / HTML here */}
     </Template>
   )
