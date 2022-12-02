819e531f1b1a99aec10e03fb6ab888adb57ed579
fupete <hereisfupete@gmail.com>
2022-12-02 17:49:30 +0100

    fix/connector js pageContext + location in realizzarae

--- src/pages/risorse-per-progettare/realizzare/sviluppo-interfaccia.js
+++ src/pages/risorse-per-progettare/realizzare/sviluppo-interfaccia.js
@@ -4,15 +4,15 @@ import {Seo} from "../../../components/seo/seo"
 
 import Pagedata from "./sviluppo-interfaccia.yaml"
 
-const Level4 = () => {
+const Level3 = ({ pageContext,location }) => {
   return(
-    <Template Pagedata={Pagedata}>
+    <Template Pagedata={Pagedata} pageContext={pageContext} location={location}>
       {/* place extra components / HTML here */}
     </Template>
   )
 }
 
-export default Level4
+export default Level3
 
 export const Head = () => (
 	<Seo
