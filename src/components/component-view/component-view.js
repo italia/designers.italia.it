7a1cb4fe7f6f1faf8934bbb1af12af0237eaa4ea
Gabriele Peirolo <g.peirolo@wedoo.it>
2022-12-06 14:57:31 +0100

    feat: component view accordion label and button

--- src/components/component-view/component-view.js
+++ src/components/component-view/component-view.js
@@ -1,19 +1,21 @@
 import React from "react"
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from 'uuid'
 
 //import DOMPurify from 'isomorphic-dompurify'
-import sanitizeHtml from 'sanitize-html';
+import sanitizeHtml from 'sanitize-html'
 import { lucario } from 'react-syntax-highlighter/dist/esm/styles/prism'
-import Button from "../button/button";
-import loadable from "@loadable/component";
+import Button from "../button/button"
+import Icon from "../icon/icon"
+import loadable from "@loadable/component"
 
-const SyntaxHighlighter = loadable(() => import('./syntax-highlighter'));
+const SyntaxHighlighter = loadable(() => import('./syntax-highlighter'))
 
 const ComponentView = ({
   content,
   viewer,
-  codeLabel,
-  codeUrl,
+  accordionLabel,
+  accordionUrl,
+  accordionSrLabel
 }) => {
 
   const createMarkup = (html) => {
@@ -23,13 +25,20 @@ const ComponentView = ({
     }) };
   }
 
+  const ICON_EXTERNAL = {
+    icon: "sprites.svg#it-external-link",
+    size: "sm",
+    color: "primary",
+    addonClasses: "align-middle me-3",
+  }
+
   const uuid = 'component-view-' + uuidv4()
   const accId = uuid + '-accordion'
   const headId = uuid + '-heading'
   const collId = uuid + '-collapse'
   let responsiveButtonsItems
 
-  if (viewer.responsiveButtons) {
+  if (viewer) {
     responsiveButtonsItems = (viewer.responsiveButtons).map((item,index) => {
       return(
         <Button key={"rb"-+index} {...item}/>
@@ -50,16 +59,21 @@ const ComponentView = ({
         }
         <div dangerouslySetInnerHTML={createMarkup(content)} />
       </div>
-      <div class="accordion accordion-left-icon" id={accId}>
-        <div class="accordion-item">
-          <h3 class="accordion-header no_toc" id={headId}>
-            <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target={'#' + collId } aria-expanded="true" aria-controls={collId}>
-              {codeLabel}
+      <div className="accordion accordion-left-icon" id={accId}>
+        <div className="accordion-item">
+          <h3 className="accordion-header d-flex justify-content-between align-items-center" id={headId}>
+            <button className="accordion-button border-top-0" type="button" data-bs-toggle="collapse" data-bs-target={'#' + collId } aria-expanded="true" aria-controls={collId}>
+              {accordionLabel}
             </button>
+			      {accordionUrl &&
+              <a href={accordionUrl} target="_blank" aria-label={accordionSrLabel}>
+                <Icon {...ICON_EXTERNAL}/>
+              </a>
+            }
           </h3>
-          {codeUrl && <a href={codeUrl} title="">{codeUrl}</a>}
-          <div id={collId} class="accordion-collapse collapse show" data-bs-parent={'#' + accId} role="region" aria-labelledby={headId}>
-            <div class="accordion-body p-0">
+
+          <div id={collId} className="accordion-collapse collapse show" data-bs-parent={'#' + accId} role="region" aria-labelledby={headId}>
+            <div className="accordion-body p-0">
               <SyntaxHighlighter language="markup" style={lucario} showLineNumbers={true}>
                 {content}
               </SyntaxHighlighter>
--- src/components/tab/tab.js
+++ src/components/tab/tab.js
@@ -16,7 +16,7 @@ const Tab = ({
   return (
     <div className="tab pt-5 pt-lg-6">
       <ul className="nav nav-tabs nav-tabs-cards" id="card-simple" role="tablist">
-      <li className="nav-item-filler flex-grow-0"></li>
+      <li className="nav-item-filler flex-grow-0 px-3 p-md-0"></li>
         <li className="nav-item">
           <a className="nav-link active" id="card-simple1-tab" data-bs-toggle="tab" href="#card-simpletab1" role="tab" aria-controls="card-simpletab1" aria-selected="true">{tab01.title}</a>
         </li>
@@ -26,15 +26,21 @@ const Tab = ({
         <li className="nav-item">
           <a className="nav-link" id="card-simple3-tab" data-bs-toggle="tab" href="#card-simpletab3" role="tab" aria-controls="card-simpletab3" aria-selected="false">{tab03.title}</a>
         </li>
-        <li className="nav-item-filler"></li>
+        <li className="nav-item-filler px-3 p-md-0"></li>
       </ul>
       <div className="tab-content" id="card-simpleContent">
         <div className="tab-pane py-5 fade show active" id="card-simpletab1" role="tabpanel" aria-labelledby="card-simple1-tab">
 
           {tab01.variants && <ContentSelect {...tab01.componentVariant}>
               { tab01.variants.map((v, idx) => <ContentSelectItem key={'item-' + idx} name={v.name}>
-                  <ComponentView content={v.content} viewer={tab01.componentVariant.viewer} codeLabel={tab01.componentVariant.codeLabel} codeUrl={tab01.componentVariant.codeUrl} />
-                </ContentSelectItem>) }
+                  <ComponentView
+                    content={v.content}
+                    viewer={tab01.componentVariant.viewer}
+                    accordionLabel={tab01.componentVariant.accordionLabel}
+                    accordionUrl={tab01.componentVariant.accordionUrl}
+                    accordionSrLabel={tab01.componentVariant.accordionSrLabel}
+                  />
+              </ContentSelectItem>) }
             </ContentSelect>}
 
           {tab01.sectionsEditorial && tab01.sectionsEditorial.map((section,index) => {
@@ -47,7 +53,15 @@ const Tab = ({
         <div className="tab-pane py-5 fade" id="card-simpletab2" role="tabpanel" aria-labelledby="card-simple2-tab">
 
           {tab02.variants && <ContentSelect {...tab02.componentVariant}>
-              { tab02.variants.map((v, idx) => <ContentSelectItem key={'item-' + idx} name={v.name}><ComponentView content={v.content} viewer={tab02.componentVariant.viewer}/></ContentSelectItem>) }
+              { tab02.variants.map((v, idx) => <ContentSelectItem key={'item-' + idx} name={v.name}>
+              <ComponentView
+                  content={v.content}
+                  viewer={tab02.componentVariant.viewer}
+                  accordionLabel={tab02.componentVariant.accordionLabel}
+                  accordionUrl={tab02.componentVariant.accordionUrl}
+                  accordionSrLabel={tab02.componentVariant.accordionSrLabel}
+                />
+              </ContentSelectItem>) }
             </ContentSelect>}
 
           {tab02.sectionsEditorial && tab02.sectionsEditorial.map((section,index) => {
@@ -60,7 +74,15 @@ const Tab = ({
         <div className="tab-pane py-5 fade" id="card-simpletab3" role="tabpanel" aria-labelledby="card-simple3-tab">
 
           {tab03.variants && <ContentSelect {...tab03.componentVariant}>
-              { tab03.variants.map((v, idx) => <ContentSelectItem key={'item-' + idx} name={v.name}><ComponentView content={v.content} viewer={tab03.componentVariant.viewer}/></ContentSelectItem>) }
+              { tab03.variants.map((v, idx) => <ContentSelectItem key={'item-' + idx} name={v.name}>
+                <ComponentView
+                  content={v.content}
+                  viewer={tab03.componentVariant.viewer}
+                  accordionLabel={tab03.componentVariant.accordionLabel}
+                  accordionUrl={tab03.componentVariant.accordionUrl}
+                  accordionSrLabel={tab03.componentVariant.accordionSrLabel}
+                />
+              </ContentSelectItem>) }
             </ContentSelect>}
 
           {tab03.sectionsEditorial && tab03.sectionsEditorial.map((section,index) => {
