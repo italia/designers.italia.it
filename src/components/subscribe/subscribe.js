68b6bf92d96834addc9c0b54e2af0dcbf2068c6b
Fabio Bonelli <fb@fabiobonelli.it>
2022-12-14 10:42:09 +0100

    feat: add mailing list registration (#550)
    Fix #523.

--- src/components/subscribe/subscribe.js
+++ src/components/subscribe/subscribe.js
@@ -1,28 +1,148 @@
-import React from "react"
-import './subscribe.scss'
-import Button from "../button/button"
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+
+import './subscribe.scss';
+
+const messages = {
+  it: {
+    header: 'Le nostre iniziative, direttamente nella tua mail',
+    title: 'Lasciaci la tua email e ti aggiorneremo sulle nostre prossime iniziative',
+    loading: 'Caricamento...',
+    placeholder: 'indirizzo email',
+    errorText: 'Qualcosa è andato storto 😔 Riprova più tardi',
+    successText: 'Ti abbiamo mandato un’email, segui il collegamento per confermare la tua iscrizione',
+    privacyText: 'leggi l’informativa',
+  },
+  en: {
+    header: 'Our initiatives, right in your email',
+    title: 'Leave us your email address and we’ll keep you posted',
+    loading: 'Loading...',
+    placeholder: 'email address',
+    errorText: 'Something went wrong 😔 Please try again later',
+    successText: 'We sent you an email, follow the link to confirm your subscription',
+    privacyText: 'privacy policy',
+  },
+};
+
+const StateClass = Object.freeze({
+  START: { className: '' },
+  LOADING: { className: 'success' },
+  SUCCESS: { className: 'success' },
+  ERROR: { className: 'danger' },
+});
 
 const Subscribe =({
-	label,
-	id,
-	idForm,
-	action,
-	button,
-	labelClass
-})=>{
-	let labelStyle = 'active'
-	+ `${labelClass ? ' '+labelClass : ''}`
-	return(
-		<div className="subscribe">
-			<form id={idForm} action={action}>
-				<div className="form-group mb-0 me-3">
-					<label htmlFor={id} className={labelStyle}>{label}</label>
-					<input type="email" className="form-control" id={id}></input>
-				</div>
-				<Button {...button}></Button>
-			</form>
-		</div>
-	)
+  label,
+  id,
+  idForm,
+  sendportalArgs,
+  button,
+  labelClass,
+  lang,
+}) => {
+  const [state, setState] = useState(StateClass.START);
+  const [message, setMessage] = useState('');
+
+  const t = (key) => messages[lang][key];
+
+  const onSubmit = async (e) => {
+    const endpoint = 'https://sendportal.developers.italia.it/api/v1/subscribe';
+    const data = {};
+
+    new FormData(e.target).forEach((value, key) => (data[key] = value));
+
+    e.preventDefault();
+
+    try {
+      setState(StateClass.LOADING);
+
+      const r = await fetch(endpoint, {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+      });
+
+      if (!r.ok) {
+        throw new Error(`HTTP response: ${r.status}`);
+      }
+    } catch (e) {
+      setState(StateClass.ERROR);
+      console.error(`Mailing list subscribe error: ${e}`);
+      setMessage(t('errorText'));
+
+      return;
+    }
+
+    setState(StateClass.SUCCESS);
+    setMessage(t('successText'));
+  };
+
+  return (
+    <>
+      <div className="subscribe">
+        <form id={idForm} onSubmit={onSubmit}>
+          <div className="form-group mb-0 me-3">
+            <label htmlFor={id} className={`active ${labelClass}`}>{label}</label>
+            <input
+              type="email"
+              className="form-control"
+              required
+              id={id}
+              name="email"
+            />
+            {Object.entries(sendportalArgs).map(([argName, argValue], i) => (
+              <input type="hidden" name={argName} value={argValue} key={i} />
+            ))}
+          </div>
+          <button
+            type="submit"
+            className="btn btn-outline-primary"
+            disabled={state === StateClass.LOADING}
+          >
+            {state === StateClass.LOADING ? (
+              <>
+                <span
+                  className="spinner-border spinner-border-sm me-2" role="status"
+                  aria-hidden="true"
+                />
+                <span className="sr-only">{t('loading')}</span>
+              </>
+            ) : (
+              <span>{button.label}</span>
+            )}
+          </button>
+        </form>
+        <div
+          className={`alert alert-${state.className} bg-light mt-2`}
+          style={{ visibility: state === StateClass.SUCCESS || state === StateClass.ERROR ? 'visible' : 'hidden' }}
+          role="alert"
+        >
+          {message}
+        </div>
+      </div>
+    </>
+  );
 }
+
+Subscribe.propTypes = {
+  label: PropTypes.string.isRequired,
+  labelClass: PropTypes.string,
+  id: PropTypes.string.isRequired,
+  idForm: PropTypes.string.isRequired,
+  button: PropTypes.object.isRequired,
+  lang: PropTypes.string,
+  sendportalArgs: PropTypes.object,
+};
+
+Subscribe.defaultProps = {
+  labelClass: '',
+  sendportalArgs: '{}',
+  lang: 'it',
+};
+
+Subscribe.displayName = 'Subscribe';
 
 export default Subscribe
