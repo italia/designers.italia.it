7c08efc3a48dfcecdf72ee12130d64cffe62a9eb
fupete <hereisfupete@gmail.com>
2022-12-15 18:17:28 +0100

    fix/a11y aria + links footer

--- src/components/footer-small/footer-small.js
+++ src/components/footer-small/footer-small.js
@@ -17,6 +17,7 @@ const FooterSmall = ({
                 <Link
                   to={value.url}
                   target={value.blank ? "_blank" : undefined}
+                  aria-label={value.ariaLabel}
                 >{value.title}</Link>
               </li>
             )
