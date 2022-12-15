ccfaa8e8ff256c39939bc542fefa7c0158287381
fupete <hereisfupete@gmail.com>
2022-12-15 15:58:32 +0100

    fix/simpify markup

--- src/components/header-pre/header-pre.js
+++ src/components/header-pre/header-pre.js
@@ -4,19 +4,13 @@ import Link from "../link/link"
 const HeaderPre = ({data}) => {
 
 	return (
-		<div className=" p-0">
-			<div className="header-pre bg-light py-2 small">
+			<div className="header-pre bg-white py-2 small">
 				<div className="container-xxl">
-					<div className="row">
-						<div className="col-12">
-							<div className="text-center">
-							<span className="fw-semibold">{data.title}</span> - <Link to={data.url}>{data.link}</Link>
-							</div>
-						</div>
+					<div className="text-center">
+						<span className="fw-semibold">{data.title}</span> - <Link to={data.url}>{data.link}</Link>
 					</div>
 				</div>
 			</div>
-		</div>
 	)
 }
 
