1bf878de510f86e126d42ec5eb280137907bc3b2
Andrea Stagi <stagi.andrea@gmail.com>
2022-12-16 16:48:35 +0100

    fix: restore navbar

---
+++ src/pages/design-system/componenti/iframes/accordion.js
@@ -0,0 +1,9 @@
+import * as React from "react"
+
+const AccordionIframe = ({ pageContext, location }) => {
+  return(
+    <button type="button" class="btn btn-primary">Primary</button>
+  )
+}
+
+export default AccordionIframe
