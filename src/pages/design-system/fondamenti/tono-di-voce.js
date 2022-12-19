d3cda082ef1eb19e6e4252d126609a5d1b7d767f
fupete <hereisfupete@gmail.com>
2022-12-19 16:45:38 +0100

    fondamenti ds + fixes

--- src/pages/design-system/fondamenti/tono-di-voce.js
+++ src/pages/design-system/fondamenti/tono-di-voce.js
@@ -2,7 +2,7 @@ import * as React from "react"
 import Template from "../../../templates/tmpl-design-system-index"
 import {Seo} from "../../../components/seo/seo"
 
-import Pagedata from "./fondamento.yaml"
+import Pagedata from "./tono-di-voce.yaml"
 
 const DesignSystemFondamento = ({ pageContext,location }) => {
   return(
