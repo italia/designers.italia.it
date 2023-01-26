a41b50be80086dd23d1df82f497b3c544156c31e
Fabio Bonelli <fb@fabiobonelli.it>
2023-01-26 13:24:14 +0100

    wip

--- src/pages/design-system/componenti/iframes/accordion.js
+++
@@ -1,9 +0,0 @@
-import * as React from "react"
-
-const AccordionIframe = ({ pageContext, location }) => {
-  return(
-    <button type="button" class="btn btn-primary">Primary</button>
-  )
-}
-
-export default AccordionIframe
---
+++ src/pages/{content.relativePath}.js
@@ -0,0 +1,40 @@
+import * as React from "react"
+import { graphql } from "gatsby"
+
+import jsyaml from "js-yaml"
+
+import Template from "../templates/tmpl-level-1-community"
+import { Seo } from "../components/seo/seo"
+
+const Page = ({ pageContext, location, data: { content } }) => {
+  const Pagedata = jsyaml.load(content.yaml)
+
+  return(
+    <Template Pagedata={Pagedata} pageContext={pageContext} location={location}>
+      {/* place extra components / HTML here */}
+    </Template>
+  )
+}
+
+export const query = graphql`
+ query ($id: String!) {
+   content(id: { eq: $id }) {
+     id
+     yaml
+   }
+ }
+`
+
+export default Page
+
+// export const Head = () => (
+// 	<Seo
+//     title = {Pagedata.seo.name}
+//     description = {Pagedata.seo.description}
+//     image = {Pagedata.seo.image}
+//     twitterImage = {Pagedata.seo.twitterImage}
+//     pathname = {Pagedata.seo.pathname}
+//     canonical = {Pagedata.seo.canonical}
+//   >
+//   </Seo>
+// )
