9d9eb5a49b9ad599559625473636bb538264649d
Andrea Stagi <stagi.andrea@gmail.com>
2023-02-03 10:08:10 +0100

    feat: switch template based on content

--- src/pages/{content.relativePath}.js
+++ src/pages/{content.relativePath}.js
@@ -3,12 +3,32 @@ import { graphql } from "gatsby"
 
 import jsyaml from "js-yaml"
 
-import Template from "../templates/tmpl-level-3"
+import TemplateBase from "../templates/tmpl-base"
+import TemplateDSDetail from "../templates/tmpl-design-system-detail"
+import TemplateDSIndex from "../templates/tmpl-design-system-index"
+import TemplateHome from "../templates/tmpl-home"
+import TemplateLV1Community from "../templates/tmpl-level-1-community"
+import TemplateLV1 from "../templates/tmpl-level-1"
+import TemplateLV2 from "../templates/tmpl-level-2"
+import TemplateLV3 from "../templates/tmpl-level-3"
+import TemplateLV4 from "../templates/tmpl-level-4"
 import { Seo } from "../components/seo/seo"
+
+const TEMPLATES = {
+  'community' : TemplateLV1Community,
+  'level1' : TemplateLV1,
+  'level2' : TemplateLV2,
+  'level3' : TemplateLV3,
+  'level4' : TemplateLV4,
+  'base' : TemplateBase,
+  'home' : TemplateHome,
+  'design-system-detail' : TemplateDSDetail,
+  'design-system-index' : TemplateDSIndex
+}
 
 const Page = ({ pageContext, location, data: { content } }) => {
   const Pagedata = jsyaml.load(content.yaml)
-
+  const Template = Pagedata.metadata.template ? TEMPLATES[Pagedata.metadata.template.name] : TemplateBase
   return(
     <Template Pagedata={Pagedata} pageContext={pageContext} location={location}>
       {/* place extra components / HTML here */}
