04018a34d7aa336c6907dd2564f9735145ef04bc
Andrea Stagi <stagi.andrea@gmail.com>
2023-02-03 11:28:12 +0100

    fix: tentative homepage restore

--- src/templates/tmpl-home.js
+++ src/templates/tmpl-home.js
@@ -19,6 +19,16 @@ import HeaderData from "../data/header.yaml"
 import FooterData from "../data/footer.yaml"
 import skipLinksData from "../data/skiplinks.yaml"
 
+import Highlight from "../components/highlight/highlight"
+import SearchMain from "../components/search-main/search-main"
+import ContentCollapse from "../components/content-collapse/contentCollapse"
+import SectionIntro from "../components/section-intro/section-intro"
+import HighlightCards from "../components/highlight-cards/highlight-cards"
+import ImgFull from "../components/img-full/img-full"
+import Testimonials from "../components/testimonials/testimonials"
+import BannerTextCta from "../components/banner-text-cta/banner-text-cta"
+import Numbers from "../components/numbers/numbers"
+import ImageIcons from "../components/image-icons/image-icons"
 
 const Template = ({children,Pagedata,pageContext,location}) => {
 	return (
@@ -34,7 +44,44 @@ const Template = ({children,Pagedata,pageContext,location}) => {
       </Header>
       <HeaderPost data={HeaderData.headerPost}/>
       <main id="main">
-        {children}
+        <Highlight {...Pagedata.components.hero}>
+          {Pagedata.components.hero.moreText && <ContentCollapse label={Pagedata.components.hero.moreButton} labelClose={Pagedata.components.hero.moreButtonClose}>
+            {Pagedata.components.hero.moreText}
+          </ContentCollapse>}
+        </Highlight>
+        {Pagedata.components.searchMain && <SearchMain {...Pagedata.components.searchMain}></SearchMain>}
+        {Pagedata.components.sectionIntro && <SectionIntro {...Pagedata.components.sectionIntro}></SectionIntro>}
+        {Pagedata.components.highLights && Pagedata.components.highLights.map((hl,index) => {
+          return(
+            <Highlight key={"hl-"+index} {...hl}/>
+          )
+        })}
+        {Pagedata.components.highlightCards &&
+          <HighlightCards {...Pagedata.components.highlightCards}></HighlightCards>
+        }
+
+        {Pagedata.components.imageIcons &&
+          <ImageIcons {...Pagedata.components.imageIcons}></ImageIcons>
+        }
+
+        {Pagedata.components.sectionIntroImg &&
+          <SectionIntro {...Pagedata.components.sectionIntroImg}>
+            {Pagedata.components.sectionIntroImg.testimonials &&
+              <div className="">
+                <Testimonials {...Pagedata.components.sectionIntroImg.testimonials}></Testimonials>
+              </div>
+            }
+          </SectionIntro>}
+        {Pagedata.components.highlightCards2 &&
+          <HighlightCards {...Pagedata.components.highlightCards2}></HighlightCards>
+        }
+        {Pagedata.components.bannerTextCta &&
+            <BannerTextCta {...Pagedata.components.bannerTextCta}>
+              {Pagedata.components.bannerTextCta.numbers &&
+                <Numbers {...Pagedata.components.bannerTextCta.numbers}></Numbers>
+              }
+            </BannerTextCta>
+        }
         {Pagedata.lastUpdate && <LastUpdate {...Pagedata.lastUpdate} {...location} {...pageContext}/>}
         <Feedback/>
       </main>
