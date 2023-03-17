2b4468403dfd90dc4b108fa9c8b0883ce52fd207
Daniele T <Fupete@users.noreply.github.com>
2023-03-17 11:26:30 +0100

    Merge branch 'next' into feat/standard-actions-4-4

--- gatsby-config.js
+++ gatsby-config.js
@@ -84,6 +84,12 @@ module.exports = {
         matomoPhpScript: "matomo.php",
         matomoJsScript: "matomo.js",
         enableJSErrorTracking: true,
+        additionalTrackers: [
+          {
+            siteId: `eO35WAwqon`,
+            trackerUrl: `https://ingestion.webanalytics.italia.it/matomo.php`,
+          },
+        ],
       },
     },
     {
@@ -101,7 +107,151 @@ module.exports = {
         // for advanced users.
         //
         // Note: Only the flexsearch engine supports options.
-        engineOptions: "speed",
+        engineOptions: {
+          profile: "score",
+          charset: "latin:advanced",
+          // encode: "extra",
+          tokenize: "strict", // < strict = full word (better performance) | forward or reverse partials... 
+          threshold: 11,
+          resolution: 22,
+          depth: 2,
+          filter: [
+            // XXX we need a stemmer also... 
+            // array stopwords italian > https://www.ranks.nl/stopwords/italian
+            "a",
+            "adesso",
+            "ai",
+            "al",
+            "alla",
+            "allo",
+            "allora",
+            "altre",
+            "altri",
+            "altro",
+            "anche",
+            "ancora",
+            "avere",
+            "aveva",
+            "avevano",
+            "ben",
+            "buono",
+            "che",
+            "chi",
+            "comprare",
+            "con",
+            "consecutivi",
+            "consecutivo",
+            "cosa",
+            "cui",
+            "da",
+            "del",
+            "della",
+            "dello",
+            "dentro",
+            "deve",
+            "devo",
+            "di",
+            "doppio",
+            "due",
+            "e",
+            "ecco",
+            "fare",
+            "fine",
+            "fino",
+            "fra",
+            "gente",
+            "giu",
+            "ha",
+            "hai",
+            "hanno",
+            "ho",
+            "il",
+            "indietro", 
+            "invece",
+            "io",
+            "la",
+            "le",
+            "lei",
+            "lo",
+            "loro",
+            "lui",
+            "lungo",
+            "ma",
+            "me",
+            "meglio",
+            "molta",
+            "molti",
+            "molto",
+            "nei",
+            "nella",
+            "no",
+            "noi",
+            "nome",
+            "nostro",
+            "nove",
+            "nuovi",
+            "nuovo",
+            "o",
+            "oltre",
+            "ora",
+            "otto",
+            "peggio",
+            "pero",
+            "persone",
+            "piu",
+            "poco",
+            "primo",
+            "promesso",
+            "qua",
+            "quarto",
+            "quasi",
+            "quattro",
+            "quello",
+            "questo",
+            "qui",
+            "quindi",
+            "quinto",
+            "rispetto",
+            "sara",
+            "secondo",
+            "sei",
+            "sembra",	
+            "sembrava",
+            "senza",
+            "sette",
+            "sia",
+            "siamo",
+            "siete",
+            "solo",
+            "sono",
+            "sopra",
+            "soprattutto",
+            "sotto",
+            "stati",
+            "stato",
+            "stesso",
+            "su",
+            "subito",
+            "sul",
+            "sulla",
+            "tanto",
+            "te",
+            "tempo",
+            "terzo",
+            "tra",
+            "tre",
+            "triplo",
+            "ultimo",
+            "un",
+            "una",
+            "uno",
+            "va",
+            "vai",
+            "voi",
+            "volte",
+            "vostro",
+          ],
+        },
 
         // GraphQL query used to fetch all data for the search index. This is
         // required.
@@ -112,9 +262,43 @@ module.exports = {
               node {
                 id
                 relativePath
+                metadata {
+                  template {
+                    name
+                  }
+                }
+                seo {
+                  name
+                  description
+                  pathname
+                }
                 components {
+                  hero {
+                    title
+                    subtitle
+                    text
+                    tag {
+                      label
+                    }
+                  }
+                  sectionIntro {
+                    title
+                    text
+                    moreText
+                  }
                   sectionsEditorial {
+                    title
+                    text
                     components {
+                      title
+                      text
+                    }
+                  }
+                  sectionsEditorial2 {
+                    title
+                    text
+                    components {
+                      title
                       text
                     }
                   }
@@ -129,14 +313,12 @@ module.exports = {
 
         // List of keys to index. The values of the keys are taken from the
         // normalizer function below.
-        index: [
-          "textContent",
-        ],
+        index: ["title", "description", "body"],
 
         // List of keys to store and make available in your UI. The values of
         // the keys are taken from the normalizer function below.
         // Default: all fields
-        store: ["relativePath", "textContent"],
+        store: ["id", "template", "relativePath", "path", "title", "description", "tag"],
 
         // Function used to map the result from the GraphQL query. This should
         // return an array of items to index in the form of flat objects
@@ -146,8 +328,20 @@ module.exports = {
           data.allContent.edges.map((edge) => {
             return {
               id: edge.node.id,
-              relativePath: edge.node.relativePath,
-              textContent: edge.node.components?.sectionsEditorial?.map(s => s.components?.map(c => c.text)).join(" "),
+              template: edge.node.metadata?.template?.name,
+              relativePath: "/" + edge.node.relativePath,
+              path: `${edge.node.seo?.pathname}`,
+              title: `${edge.node.components?.hero?.title}`,
+              description: `${edge.node.seo?.description}`,
+              tag: `${edge.node.components?.hero?.tag?.label}`,
+              body:  `${edge.node.components?.hero?.subtitle} ${edge.node.components?.hero?.text}` 
+                + ' ' + `${edge.node.components?.sectionIntro?.title} ${edge.node.components?.sectionIntro?.text} ${edge.node.components?.sectionIntro?.moreText}` 
+                + ' ' + edge.node.components?.sectionsEditorial?.map(s => s.title).join(' ')
+                + ' ' + edge.node.components?.sectionsEditorial?.map(s => s.components?.map(c=> c.title)).join(' ')
+                + ' ' + edge.node.components?.sectionsEditorial2?.map(s => s.components?.map(c => c.title)).join(' ')
+                + ' ' + edge.node.components?.sectionsEditorial?.map(s => s.text).join(' ')
+                + ' ' + edge.node.components?.sectionsEditorial?.map(s => s.components?.map(c => c.text)).join(' ') 
+                + ' ' + edge.node.components?.sectionsEditorial2?.map(s => s.components?.map(c => c.text)).join(' '),
             }
           }),
       },
