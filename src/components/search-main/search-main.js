80aa4b9e897d161d085641a5dc6b5cb54de57495
Daniele T <Fupete@users.noreply.github.com>
2023-03-18 11:44:33 +0100

    feat: search styling enhancement 🔎

--- src/components/search-main/search-main.js
+++ src/components/search-main/search-main.js
@@ -92,22 +92,20 @@ const SearchMain =({
                 </div>
                 : null
               }
-
-               {suggest &&
-              <div className="suggest-wrapper d-lg-flex">
-                <h3 className="mb-4">{suggest.title}</h3>
-                {suggest.items && <div className="items-wrapper d-flex flex-wrap ms-lg-5">
-                  <ul className="list-inline d-flex flex-wrap">
-                    {suggest.items.map((item, index) => (
-                      <li className="list-item me-4 mb-3" key={index}>
-                        <Button onClick={() => { setInput(item.label); search(item.label); }} type="submit" size="md" btnStyle="outline-secondary">
-                          {item.label}
-                        </Button>
-                      </li>
-                    ))}
-                  </ul>
-                </div>}
-              </div>
+              {suggest && <div className="suggest-wrapper d-lg-flex mb-3">
+                  <h3 className="mb-4">{suggest.title}</h3>
+                  {suggest.items && <div className="items-wrapper d-flex flex-wrap ms-lg-5">
+                    <ul className="list-inline d-flex flex-wrap">
+                      {suggest.items.map((item, index) => (
+                        <li className="list-item me-3 mb-3" key={index}>
+                          <Button onClick={() => { setInput(item.label); search(item.label); }} type="submit" size="md" btnStyle="outline-secondary">
+                            {item.label}
+                          </Button>
+                        </li>
+                      ))}
+                    </ul>
+                  </div>}
+                </div>
               }
               <div className="search-form px-4 pb-4 mb-5 shadow-lg">
                 <form id={formId} onSubmit={formSubmit}>
@@ -131,41 +129,43 @@ const SearchMain =({
                         <Button type="submit" {...button} />
                       </div>
                     </div>
-                    <div className="it-list-wrapper">
-                      <div  className="fw-normal text-muted">
-                        <div class="live-region" tabIndex="-1" ref={liveRegionRef}> 
-                          {(formSubmitted) && (results.length > 0) &&
-                              <div className="mt-2 ps-4 pt-4"><p>Di seguito i migliori risultati per "<strong><mark>{storedInput}</mark></strong>":</p></div>
-                          }
-                          {(formSubmitted) && (results.length === 0) &&
-                              <div className="mt-2 ps-4 pt-4"><p>Non ci sono risultati utili per "<strong><mark>{storedInput}</mark></strong>", possiamo aiutarti in altro modo?</p></div>
+                    {(results.length > 0 || formSubmitted) && <div id="results">
+                      <div className="it-list-wrapper">
+                        <div  className="fw-normal text-muted">
+                          <div class="live-region" tabIndex="-1" ref={liveRegionRef}> 
+                            {(formSubmitted) && (results.length > 0) &&
+                                <div className="mt-2 ps-4 pt-4"><p>Di seguito i migliori risultati per "<strong><mark>{storedInput}</mark></strong>":</p></div>
+                            }
+                            {(formSubmitted) && (results.length === 0) &&
+                                <div className="mt-2 ps-4 pt-4"><p>Non ci sono risultati utili per "<strong><mark>{storedInput}</mark></strong>", possiamo aiutarti in altro modo?</p></div>
+                            }
+                          </div>
+                        </div>
+                        <ul className="it-list mt-5 mt-md-3">
+                          {results.map( result => (
+                            <ListItem url={result.relativePath} key={result.id} iconLeft icon={iconOpt} addonClasses="align-items-start border-bottom-0 pt-3 px-0 px-sm-2 px-md-4">
+                              <div className="d-md-flex">
+                                <h3 className="h6 mb-0">
+                                <strong>{result.title}</strong>
+                                </h3>
+                                <div>
+                                  {(result.tag !== "undefined") ? <div className="mb-2 mt-1 mb-md-0 mt-md-0"><Tag label={result.tag} addonClasses="ms-md-4 text-uppercase px-2 py-0 fw-normal"></Tag></div> : null}
+                                  {(result.template === 'level1' || result.template === 'community') ? <div className="mb-2 mt-1 mb-md-0 mt-md-0 d-table d-sm-table d-md-inline-block "><Tag label="Panoramica" addonClasses="ms-md-4 text-uppercase bg-primary px-2 py-0 fw-normal"></Tag></div> : null}
+                                </div>
+                              </div>
+                              <p className="text-secondary fw-normal d-block mb-3">
+                                {(result.description !== null) ? <small>{result.description}</small> : null}
+                              </p>
+                            </ListItem>
+                          ))}
+                          </ul>
+                          {(results.length > 4 && !isResultsPage) &&
+                            <div className="mt-4 ps-4 pt-4 d-block border-top">
+                              <strong><Link to="/ricerca/" state={{ searchTerm: input }}>Scopri più risultati</Link></strong>
+                            </div>
                           }
                         </div>
-                      </div>
-                      <ul className="it-list mt-5 mt-md-3">
-                        {results.map( result => (
-                          <ListItem url={result.relativePath} key={result.id} iconLeft icon={iconOpt} addonClasses="align-items-start border-bottom-0 pt-3 px-0 px-sm-2 px-md-4">
-                            <div className="d-md-flex">
-                              <h3 className="h6 mb-0">
-                               <strong>{result.title}</strong>
-                              </h3>
-                              <div>
-                                {(result.tag !== "undefined") ? <div className="mb-2 mt-1 mb-md-0 mt-md-0"><Tag label={result.tag} addonClasses="ms-md-4 text-uppercase px-2 py-0 fw-normal"></Tag></div> : null}
-                                {(result.template === 'level1' || result.template === 'community') ? <div className="mb-2 mt-1 mb-md-0 mt-md-0 d-table d-sm-table d-md-inline-block "><Tag label="Panoramica" addonClasses="ms-md-4 text-uppercase bg-primary px-2 py-0 fw-normal"></Tag></div> : null}
-                              </div>
-                            </div>
-                            <p className="text-secondary fw-normal d-block mb-3">
-                               {(result.description !== null) ? <small>{result.description}</small> : null}
-                            </p>
-                          </ListItem>
-                        ))}
-                        {(results.length > 4 && !isResultsPage) &&
-                          <div className="mt-4 ps-4 pt-4 d-block border-top">
-                            <strong><Link to="/ricerca/" state={{ searchTerm: input }}>Scopri più risultati</Link></strong>
-                          </div>
-                        }
-                      </ul>
-                    </div>
+                    </div>}
                 </form>
               </div>
             </div>
