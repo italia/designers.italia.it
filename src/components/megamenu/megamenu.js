f7103d8406858a14ef7cce1863c8d8e6ddd4bb0b
Daniele T <Fupete@users.noreply.github.com>
2023-03-27 09:48:54 +0200

    fix: megamenu order-first column just on desktpop

--- src/components/megamenu/megamenu.js
+++ src/components/megamenu/megamenu.js
@@ -13,51 +13,51 @@ const Megamenu = ({
 	cols
 }) => {
 
-  const GATSBY_ACTIVE = "active"
+	const GATSBY_ACTIVE = "active"
 
-	return(
+	return (
 		<div className="megamenu pb-5 pb-lg-0">
 			<div className="row /*align-items-center*/">
-					<div className="col-12 col-lg-8">
-						{heading &&
-              <Link className="heading-link d-flex justify-content-between align-items-center" to={heading.url} activeClassName={GATSBY_ACTIVE}>
-                <span>{heading.label}</span>
-                <Icon icon="sprites.svg#it-arrow-right" size="sm" color="primary"></Icon>
-              </Link>
-						}
-						{cols &&
-							<div className="row">
-								{cols.map((col,index)=>{
-									return(
-										<div  key={"megalist-"+index} className="col-12 col-lg-6">
-											<List {...col} isDropdown="true"></List>
+				<div className="col-xs-12 col-lg-4 px-0">
+					<div className="row">
+						{info &&
+							<div className="col-12 it-vertical it-description pb-lg-3">
+								<div className="description-content img-max-megamenu ps-4 ps-sm-5 ms-3">
+									{info.img &&
+										<div className="ratio ratio-megamenu lightgrey-bg-a1 mb-4 rounded">
+											{/*<img className="rounded" src={info.img} alt={info.imgAlt}/>*/}
+											<ImageResponsive className="rounded" src={info.img} alt={info.imgAlt} loading="eager" />
 										</div>
-									)
-								})}
+									}
+									{info.text &&
+										<ReactMarkdown>{info.text}</ReactMarkdown>
+									}
+								</div>
 							</div>
 						}
-					</div>
-          <div className="col-xs-12 col-lg-4 px-0">
-				  <div className="row">
-					{info &&
-						<div className="col-12 it-vertical it-description pb-lg-3">
-							<div className="description-content img-max-megamenu ps-4 ps-sm-5 ms-3">
-								{info.img &&
-									<div className="ratio ratio-megamenu lightgrey-bg-a1 mb-4 rounded">
-										{/*<img className="rounded" src={info.img} alt={info.imgAlt}/>*/}
-										<ImageResponsive className="rounded" src={info.img} alt={info.imgAlt} loading="eager"/>
-									</div>
-								}
-								{info.text &&
-									<ReactMarkdown>{info.text}</ReactMarkdown>
-								}
-							</div>
-						</div>
-					}
-					</div>
 					</div>
 				</div>
+				<div className="col-12 col-lg-8 order-first">
+					{heading &&
+						<Link className="heading-link d-flex justify-content-between align-items-center" to={heading.url} activeClassName={GATSBY_ACTIVE}>
+							<span>{heading.label}</span>
+							<Icon icon="sprites.svg#it-arrow-right" size="sm" color="primary"></Icon>
+						</Link>
+					}
+					{cols &&
+						<div className="row">
+							{cols.map((col, index) => {
+								return (
+									<div key={"megalist-" + index} className="col-12 col-lg-6">
+										<List {...col} isDropdown="true"></List>
+									</div>
+								)
+							})}
+						</div>
+					}
+				</div>
 			</div>
+		</div>
 	)
 }
 
