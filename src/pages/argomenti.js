d82c28826b3593b6d5512abdc117bd17d8ea1d72
Daniele T <Fupete@users.noreply.github.com>
2023-03-28 18:04:33 +0200

    feat: all argomenti page list layout

--- src/pages/argomenti.js
+++ src/pages/argomenti.js
@@ -4,22 +4,25 @@ import kebabCase from "lodash/kebabCase"
 import "../scss/styles.scss"
 import "../js/globals"
 
-import Link from "../components/link/link"
+import ListItem from "../components/list-item/list-item"
+import Chip from "../components/chip/chip"
 import { graphql } from "gatsby"
 
 const TagsPage = ({data: { allContent: { group }}}) => (
-  <div>
+  <div className="mx-5 my-5">
     <h1>Tutti gli argomenti</h1>
     <p className="lead">Lista temporanea per testare il funzionamento archivi per argomento</p>
-    <ul>
-      {group.map(tag => (
-          <li key={tag.fieldValue}>
-            <Link to={`/argomenti/${kebabCase(tag.fieldValue)}/`}>
-              {tag.fieldValue} ({tag.totalCount})
-            </Link>
-          </li>
-        ))}
-    </ul>
+    <div className="chips-list-wrapper">
+        <ul className="chips-list chips mt-4 mt-md-3 d-flex flex-wrap">
+        {group.map(tag => (
+          <ListItem key={tag.fieldValue} addonClasses="align-items-start border-bottom-0 pt-3 px-0 px-sm-2 px-md-4">
+              <Chip label={tag.fieldValue} size="lg" color="primary">
+                <span className="ms-2 badge neutral-2-bg text-secondary my-0 pb-1">{tag.totalCount}</span>
+              </Chip> 
+          </ListItem>
+          ))}
+        </ul>
+    </div>
   </div>
 )
 
