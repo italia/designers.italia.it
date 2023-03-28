338356a981a8476b1dbf74b4381a6feddbc959db
Daniele T <Fupete@users.noreply.github.com>
2023-03-28 17:12:58 +0200

    feat: tag page list layout

--- src/templates/tag.js
+++ src/templates/tag.js
@@ -4,31 +4,70 @@ import "../scss/styles.scss"
 import "../js/globals"
 
 import Link from "../components/link/link"
+import ListItem from "../components/list-item/list-item"
+import Tag from "../components/tag/tag"
 import { graphql } from "gatsby"
 
 const Tags = ({pageContext, data }) => {
   const { tag } = pageContext
   const { edges, totalCount } = data.allContent
-  const tagHeader = `${totalCount} post${
-    totalCount === 1 ? "" : "s"
-  } tagged with "${tag}"`
+  const tagHeader = `${totalCount} pagin${
+    totalCount === 1 ? "a" : "e"
+  } con argomento "${tag}"`
+
+  const iconOpt = {
+    icon: 'sprites.svg#it-file',
+    size: 'sm',
+    color: "primary",
+    addonClasses: 'mt-1 flex-shrink-0 me-1 me-md-3'
+  }
 
   return (
     <div>
       <h1>{tagHeader}</h1>
-      <ul>
+      <div className="it-list-wrapper">
+        <ul className="it-list mt-4 mt-md-3">
         {edges.map(({ node }) => {
+          const {id} = node
           const {pathname} = node.seo
-          const {title} = node.components.hero
+          const {title} = node.components?.hero
+          const {description} = node.seo
           return (
-            <li key={pathname}>
-              <Link to={pathname}>{title}</Link>
-            </li>
+            <ListItem url={pathname} key={id} iconLeft icon={iconOpt} addonClasses="align-items-start border-bottom-0 pt-3 px-0 px-sm-2 px-md-4">
+            <div className="d-md-flex">
+              <h3 className="h6 mb-0">
+              <strong>{title}</strong>
+              </h3>
+              <div>
+              {node.components?.hero?.tag?.label && 
+                <div className="mb-2 mt-1 mb-md-0 mt-md-0">
+                  <Tag label={node.components?.hero?.tag?.label} addonClasses="ms-md-4 text-uppercase px-2 py-0 fw-normal"></Tag>
+                </div>
+              }
+              {node.metadata?.template?.name && 
+                (node.metadata?.template?.name === 'level1' || node.metadata?.template?.name === 'community') ? 
+                  <div className="mb-2 mt-1 mb-md-0 mt-md-0 d-table d-sm-table d-md-inline-block ">
+                    <Tag label="Panoramica" addonClasses="ms-md-4 text-uppercase bg-primary px-2 py-0 fw-normal"></Tag>
+                    </div> 
+                    : null}
+                  </div>
+            </div>
+            {(node.components?.hero?.kangaroo?.personalInfo?.items || description) && 
+              <p className="text-secondary fw-normal d-block mb-3">
+                {node.components?.hero?.kangaroo?.personalInfo?.items && 
+                <small>{node.components?.hero?.kangaroo?.personalInfo?.items[1].label}</small>} {/*// XXX WE NEED AN UNIVERSAL NEWS DATE FIELD */}
+                <small> — {description}</small>
+              </p>
+            }
+            </ListItem>
           )
         })}
       </ul>
-      <Link to="/argomenti">All tags</Link>
+      <div class="my-5 mx-5">
+        <Link to="/argomenti">Scopri tutti gli argomenti</Link>
+      </div>
      </div>
+    </div>
   )
 }
 
@@ -38,16 +77,35 @@ export const pageQuery = graphql`
   query($tag: String) {
     allContent(
       filter: {components: {hero: {kangaroo: {tags: {in: [$tag]}}}}}
+      sort: {order: ASC, fields: components___hero___title}
     ) {
       totalCount
       edges {
         node {
+          id
+          metadata {
+            template {
+              name
+            }
+          }
           components {
             hero {
               title
+              tag {
+                label
+              }
+              kangaroo {
+                personalInfo {
+                  items {
+                    title
+                    label
+                  }
+                }
+              }
             }
           }
           seo {
+            description
             pathname
           }
         }
