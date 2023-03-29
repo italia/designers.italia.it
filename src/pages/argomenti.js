fc840fd5bf8f19bf2a9390cc008243d20c5909a6
Daniele T <Fupete@users.noreply.github.com>
2023-03-29 11:33:13 +0200

    fix: various

--- src/pages/argomenti.js
+++ src/pages/argomenti.js
@@ -1,5 +1,4 @@
 import * as React from "react"
-import kebabCase from "lodash/kebabCase"
 
 import "../scss/styles.scss"
 import "../js/globals"
@@ -8,20 +7,20 @@ import ListItem from "../components/list-item/list-item"
 import Chip from "../components/chip/chip"
 import { graphql } from "gatsby"
 
-const TagsPage = ({data: { allContent: { group }}}) => (
+const TagsPage = ({ data: { allContent: { group } } }) => (
   <div className="mx-5 my-5">
     <h1>Tutti gli argomenti</h1>
     <p className="lead">Lista temporanea per testare il funzionamento archivi per argomento</p>
     <div className="chips-list-wrapper">
-        <ul className="chips-list chips mt-4 mt-md-3 d-flex flex-wrap">
+      <ul className="chips-list chips mt-4 mt-md-3 d-flex flex-wrap">
         {group.map(tag => (
           <ListItem key={tag.fieldValue} addonClasses="align-items-start border-bottom-0 pt-3 px-0 px-sm-2 px-md-4">
-              <Chip label={tag.fieldValue} size="lg" color="primary">
-                <span className="ms-2 badge neutral-2-bg text-secondary my-0 pb-1">{tag.totalCount}</span>
-              </Chip> 
+            <Chip label={tag.fieldValue} size="lg" color="primary">
+              <span className="ms-2 badge neutral-2-bg text-secondary my-0 pb-1">{tag.totalCount}</span>
+            </Chip>
           </ListItem>
-          ))}
-        </ul>
+        ))}
+      </ul>
     </div>
   </div>
 )
