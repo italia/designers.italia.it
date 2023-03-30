2b3c49c8f80a8fa749b8bf39660a3d28ac4defc8
Daniele T <Fupete@users.noreply.github.com>
2023-03-30 10:29:24 +0200

    feat: footer most used tags block

--- src/components/footer-main/footer-main.js
+++ src/components/footer-main/footer-main.js
@@ -6,18 +6,39 @@ import Subscribe from "../subscribe/subscribe"
 import Link from "../link/link"
 import "./footer-main.scss"
 
+import { useStaticQuery, graphql } from "gatsby"
+import Chip from "../chip/chip"
+
 const FooterMain = ({
   id,
   title,
   cols,
+  tagsNo,
   social,
   community,
   subscribe
 }) => {
+
+  const data = useStaticQuery(graphql`
+  query {
+    allContent {
+      group(field: components___hero___kangaroo___tags) {
+        fieldValue
+        totalCount
+      }
+    }
+  }
+  `)
+
+  // sort most important tags, get a fixed numbers of it, let space for the View all link on the last column
+  data.allContent.group.sort((a, b) => parseFloat(b.totalCount) - parseFloat(a.totalCount));
+  const footerTags = data.allContent.group.slice(0, tagsNo)
+  footerTags.sort((a, b) => parseFloat(b.totalCount) - parseFloat(a.totalCount));
+
   return (
     <div className="it-footer-main py-5" id={id}>
-			<div className="container-xxl">
-				<section>
+      <div className="container-xxl">
+        <section>
           {title &&
             <div className="row clearfix pb-4">
               <div className="col-12">
@@ -25,25 +46,46 @@ const FooterMain = ({
               </div>
             </div>
           }
-				</section>
-				<section>
-					<div className="row">
-            {cols.map((value,index)=>{
-              return(
-              <div key={"item-"+index} className="col-12 col-md-6 col-lg-3 col-sm-6 pb-mb-2 pb-lg-0">
-                <List {...value}>
-                {value.items.map((item,index)=>{
-                  return (
-                  <ListItem {...item} key={"subitem-"+index}></ListItem>
-                  )
-                })}
-                </List>
+        </section>
+        <section>
+
+          {footerTags && <div className="row">
+            <div className="chips-list-wrapper">
+              <ul className="chips-list chips d-flex flex-wrap">
+                {footerTags.map(tag => (
+                  <ListItem key={tag.fieldValue} addonClasses="align-items-start border-bottom-0 pt-3 px-0 px-sm-2 px-md-4">
+                    <Chip label={tag.fieldValue} size="lg" color="primary">
+                      {/* <span className="ms-2 badge neutral-2-bg text-secondary my-0 pb-1">{tag.totalCount}</span> */}
+                    </Chip>
+                  </ListItem>
+                ))}
+              </ul>
+              <div className="text-end">
+                <Link className="text-white" to="/argomenti/"><strong>Scopri tutti gli argomenti</strong></Link>
               </div>
+            </div>
+          </div>
+          }
+
+          {cols && <div className="row">
+            {cols.map((value, index) => {
+              return (
+                <div key={"item-" + index} className="col-12 col-md-6 col-lg-3 col-sm-6 pb-mb-2 pb-lg-0">
+                  <List {...value}>
+                    {value.items.map((item, index) => {
+                      return (
+                        <ListItem {...item} key={"subitem-" + index}></ListItem>
+                      )
+                    })}
+                  </List>
+                </div>
               )
             })}
-					</div>
-				</section>
-				<section className="footer-utilities mt-5 pt-5">
+          </div>
+          }
+
+        </section>
+        <section className="footer-utilities mt-5 pt-5">
           <div className="row">
 
             {community &&
@@ -51,27 +93,27 @@ const FooterMain = ({
                 <h3 className="footer-title pb-4">{community.title}</h3>
 
                 <List {...community.list}>
-                {community.list.items.map((item,index)=>{
-                  return (
-                  <ListItem {...item} key={"subitem-"+index}></ListItem>
-                  )
-                })}
+                  {community.list.items.map((item, index) => {
+                    return (
+                      <ListItem {...item} key={"subitem-" + index}></ListItem>
+                    )
+                  })}
                 </List>
               </div>
             }
 
-        {social &&
+            {social &&
               <div className="col-12 col-md-6 col-lg-3 mb-5 mb-lg-0">
                 <h3 className="footer-title pb-4">{social.title}</h3>
-                {social.buttons.map((button,index)=>{
+                {social.buttons.map((button, index) => {
                   return (
-                    <Button key={"social-"+index} {...button}></Button>
+                    <Button key={"social-" + index} {...button}></Button>
                   )
                 })}
               </div>
             }
 
-            
+
             {subscribe &&
               <div className="col-12 col-lg-6">
                 <div className="title-wrapper d-flex justify-content-between">
@@ -89,8 +131,8 @@ const FooterMain = ({
             }
           </div>
         </section>
-			</div>
-		</div>
+      </div>
+    </div>
   )
 }
 
