cdf133823f3184af6bcf55bef699f82fa20d803e
Daniele T <Fupete@users.noreply.github.com>
2023-04-07 13:10:30 +0200

    Fix: clean fixes to a bunch of build warnings

--- src/components/button/button.js
+++ src/components/button/button.js
@@ -31,7 +31,7 @@ const Button = ({
 		+ `${size ? ' btn-'+size : ''}`
 		+ `${btnStyle ? ' btn-'+btnStyle : ''}`
 		+ `${disabled ? ' disabled' : ''}`
-		+ `${(iconLeft || iconRight && !dataBsToggle) ? ' btn-icon' : ''}`
+		+ `${(iconLeft || (iconRight && !dataBsToggle)) ? ' btn-icon' : ''}`
 		+ `${customStyle ? customStyle : ''}`
 		+ `${addonStyle ? ' '+addonStyle : ''}`
 
