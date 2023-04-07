459622617f81ffcdb334976227fa1bb87ef25e94
Daniele T <Fupete@users.noreply.github.com>
2023-04-07 23:20:19 +0200

    fix: component bg_

--- src/components/component-view/component-view.js
+++ src/components/component-view/component-view.js
@@ -124,7 +124,7 @@ const ComponentView = ({
     <div id={uuid}>
       <div className={componentStyles}>
         <span className="visually-hidden">Inizio componente:</span>
-        <iframe id={`${idPrefix}-iframe`} src={BSIExampleUrl} title={`Variante: ${name}`} className={`w-100 iframe-example ${[...dynamicResClasses]}`}></iframe>
+        <iframe id={`${idPrefix}-iframe`} src={BSIExampleUrl} title={`Variante: ${name}`} className={`w-100 iframe-example bg-white ${[...dynamicResClasses]}`}></iframe>
         <span className="visually-hidden">Fine componente.</span>
         {responsiveButtonsItems &&
           <div className="responsive-buttons d-none d-lg-block">
