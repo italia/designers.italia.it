7203757e725c0374bd6f600d376dff1d76b0a694
Daniele T <Fupete@users.noreply.github.com>
2023-04-11 16:34:32 +0200

    feat: add shadow-sm

--- src/components/component-view/component-view.js
+++ src/components/component-view/component-view.js
@@ -131,7 +131,7 @@ const ComponentView = ({
     <div id={uuid}>
       <div className={componentStyles}>
         <span className="visually-hidden">Inizio componente:</span>
-        <iframe id={`${idPrefix}-iframe`} src={BSIExampleUrl} title={`Variante: ${name}`} className={`w-100 rounded border iframe-example ${previewWidth}`}></iframe>
+        <iframe id={`${idPrefix}-iframe`} src={BSIExampleUrl} title={`Variante: ${name}`} className={`w-100 rounded border shadow-sm iframe-example ${previewWidth}`}></iframe>
         <span className="visually-hidden">Fine componente.</span>
         {responsiveButtonsItems &&
           <div className="responsive-buttons d-none d-lg-block">
