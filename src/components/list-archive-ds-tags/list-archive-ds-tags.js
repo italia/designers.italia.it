69ea33c89d37694926a9920e254f073904e56bf1
Daniele T <Fupete@users.noreply.github.com>
2023-04-13 09:40:10 +0200

    fix: tags list responsiveness

--- src/components/list-archive-ds-tags/list-archive-ds-tags.js
+++ src/components/list-archive-ds-tags/list-archive-ds-tags.js
@@ -27,9 +27,9 @@ const ListArchiveDSTags = ({
 
   return (
     <section className="pb-5" aria-describedby="archive-list-title">
-      <div className="px-3 px-lg-0 px-lg-5 d-flex flex-row align-items-center">
+      <div className="px-0 px-md-3 px-lg-4 px-xl-5 d-md-flex flex-row align-items-center">
         <h3 className="border-end pe-4" id="archive-list-title">Esplora per utilizzo</h3>
-        <div className="chips-list-wrapper ps-4">
+        <div className="chips-list-wrapper ps-md-4">
           <ul className="chips-list chips mt-4 mt-md-3 d-flex flex-wrap">
             {data.tagsDesignSystem.group.map(tag => (
               <ListItem key={tag.fieldValue} addonClasses="align-items-start border-bottom-0 pt-3 px-0 px-sm-2 px-md-4">
