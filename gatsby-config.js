3682db5f5d66748704644c2c0f5f314684d685a6
Lorenzo Pappalardo <lorenzopappalardo7@gmail.com>
2023-05-01 10:28:06 +0200

    Customized eslint config to restore as many rules of the default gatsby configuration as possible

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,19 @@ require("dotenv").config({
   path: `.env.${process.env.NODE_ENV}`,
 })
 
+const path = require('path');
+// Get paths of Gatsby's required rules, which as of writing is located at:
+// https://github.com/gatsbyjs/gatsby/tree/fbfe3f63dec23d279a27b54b4057dd611dce74bb/packages/
+// gatsby/src/utils/eslint-rules
+const gatsbyRequiredRules = path.join(
+  process.cwd(),
+  'node_modules',
+  'gatsby',
+  'dist',
+  'utils',
+  'eslint-rules'
+);
+
 module.exports = {
   trailingSlash: "always",
   siteMetadata: {
@@ -150,6 +163,19 @@ module.exports = {
               textContent: edge.node.components?.sectionsEditorial?.map(s => s.components?.map(c => c.text)).join(" "),
             }
           }),
+      },
+    },
+    {
+      resolve: "gatsby-plugin-eslint",
+      options: {
+        // Gatsby required rules directory
+        rulePaths: [gatsbyRequiredRules],
+        // Default settings that may be omitted or customized
+        stages: ["develop"],
+        extensions: ["js", "jsx", "ts", "tsx"],
+        exclude: ["node_modules", "bower_components", ".cache", "public"],
+        // Any additional eslint-webpack-plugin options below
+        // ...
       },
     },
     // This must be the last plugin in the array
