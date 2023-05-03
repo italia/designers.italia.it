3401d5a400ca74755be291670c621d8478c134fb
Andrea Stagi <stagi.andrea@gmail.com>
2023-05-03 15:32:58 +0200

    chore(deps): remove local bsi

--- bootstrap-italia/dist/plugins/collapse.js
+++
@@ -1,6 +0,0 @@
-import BSCollapse from 'bootstrap/js/src/collapse';
-
-class Collapse extends BSCollapse {}
-
-export { Collapse as default };
-//# sourceMappingURL=collapse.js.map
--- bootstrap-italia/dist/plugins/tooltip.js
+++
@@ -1,6 +0,0 @@
-import BSTooltip from 'bootstrap/js/src/tooltip';
-
-class Tooltip extends BSTooltip {}
-
-export { Tooltip as default };
-//# sourceMappingURL=tooltip.js.map
