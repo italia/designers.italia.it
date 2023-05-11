2e2e84d677277a09fbffa82186720d8400ae273a
Andrea Stagi <stagi.andrea@gmail.com>
2023-05-11 23:23:27 +0200

    chore: remove script to install local bsi

--- install_local_bsi.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-rm -rf ./bootstrap-italia
-mkdir -p ./bootstrap-italia
-cp -R ../../italia/bootstrap-italia/dist ./bootstrap-italia
-cp -R ../../italia/bootstrap-italia/src ./bootstrap-italia
-cp -R ../../italia/bootstrap-italia/package.json ./bootstrap-italia
