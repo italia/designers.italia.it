e20d549f9dcf0e14fef8110d21e60aaa2ac49c61
Daniele T <Fupete@users.noreply.github.com>
2023-10-12 18:11:39 +0200

    feat: large images

--- src/components/img-full/img-full.js
+++ src/components/img-full/img-full.js
@@ -1,10 +1,21 @@
 import React from "react";
+import classNames from "classnames";
 import ImageResponsive from "../image-responsive/image-responsive";
 import "./img-full.scss";
 
-function ImgFull({ img, alt }) {
+function ImgFull({
+  img,
+  alt,
+  isDSPreview
+}) {
+  let imgStyle = classNames("img-full", {
+    "mb-5": !isDSPreview,
+    "mb-3 mt-3": isDSPreview,
+  });
+
+
   return (
-    <div className="img-full mb-5">
+    <div className={imgStyle}>
       <ImageResponsive imgClassName="w-100 img-fluid" src={img} alt={alt} />
     </div>
   );
