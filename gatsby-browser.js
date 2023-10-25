c1528a1ca7724561e25b15d62ee0e9cfb492c690
Andrea Stagi <stagi.andrea@gmail.com>
2023-10-25 16:19:05 +0200

    fix: do not update scroll

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,13 +1,3 @@
-exports.shouldUpdateScroll = ({
-  routerProps: { location },
-  getSavedScrollPosition
-}) => {
-  window.history.scrollRestoration = "manual"
-  const currentPosition = getSavedScrollPosition(location)
-
-  window.setTimeout(() => {
-    window.scrollTo(...(currentPosition || [0, 0]))
-  }, 100 )
-
+exports.shouldUpdateScroll = () => {
   return false
 }
