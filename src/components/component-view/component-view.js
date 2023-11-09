7564b800436fbcac847b03addb5b448617d8a056
Daniele T <Fupete@users.noreply.github.com>
2023-11-09 09:20:01 +0100

    fix: lint

--- src/components/component-view/component-view.js
+++ src/components/component-view/component-view.js
@@ -265,7 +265,10 @@ function ComponentView({
               aria-labelledby={headId}
             >
               <div className="accordion-body p-0 position-relative">
-                <div aria-hidden="true" className="position-absolute end-0 split-code-checkbox">
+                <div
+                  aria-hidden="true"
+                  className="position-absolute end-0 split-code-checkbox"
+                >
                   {contentTrimmed && (
                     <Checkbox
                       id={`${idPrefix}-wrap`}
