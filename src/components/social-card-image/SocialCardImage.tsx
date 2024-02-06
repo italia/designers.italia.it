bdfa5cdc95105bbf80cd3d6434c91a20db23c739
Daniele T <Fupete@users.noreply.github.com>
2024-02-06 16:50:17 +0100

    fix: styling + events pathnames

--- src/components/social-card-image/SocialCardImage.tsx
+++ src/components/social-card-image/SocialCardImage.tsx
@@ -1,5 +1,6 @@
 import { Node } from 'gatsby'
 import React from 'react'
+import { truncate } from "lodash"
 
 interface ContentMetadata {
   seo: {
@@ -14,36 +15,44 @@ export default function (node: Node) {
   if (node.internal.type === 'Content') {
     const content = node as unknown as ContentMetadata;
     const { name, description, pathname, image } = content.seo;
-    const nameSimplified = name.replace(' - Designers Italia', '') // remove handmade branding in title if present
+    // specify pathroot only below second level pages
     const pathnameSplitted = pathname.split('/');
     const pathroot = pathnameSplitted.slice(0, pathnameSplitted.length - 2).join('/');
+    // remove handmade branding in title (if present), and cut at 86 characters long/about 4 rows max
+    const nameSimplified = truncate(name.replace(' - Designers Italia', ''), {
+      'length': 86,
+    })
 
     return (
       <div style={{ display: 'flex', height: '100%', width: '100%', backgroundColor: '#fff', }}>
         <div style={{ height: '100%', width: '100%', display: 'flex', justifyContent: 'space-between', flexDirection: 'column', color: '#212121', }}>
           <div style={{ display: 'flex', width: '100%', flexDirection: 'row', justifyContent: 'space-between', alignItems: 'flex-start' }}>
-            <div style={{ display: 'flex', flexDirection: 'column', width: 940, paddingLeft: 32, paddingRight: 32, paddingTop: 32, paddingBottom: 32, }}>
+            <div style={{ display: 'flex', flexDirection: 'column', width: 924, paddingLeft: 32, paddingRight: 32, paddingTop: 32, paddingBottom: 32, }}>
               {pathroot ? <div style={{ display: 'flex', fontSize: 36, fontFamily: "tBold", fontWeight: 700, lineHeight: .9, paddingBottom: 16, }}>
                 {pathroot}
               </div> : ''}
               <div style={{ fontSize: 80, fontFamily: "tBold", fontWeight: 700, lineHeight: .9, }}>
                 {nameSimplified}
               </div>
-              {/* <div style={{ fontSize: 28, fontFamily: "tRegular", fontWeight: 400, color: '#666', paddingRight: 50, display: 'flex', flexWrap: 'wrap', alignItems: 'center', marginTop: 8, }}>
+              {/* <div style={{ fontSize: 40, fontFamily: "tRegular", fontWeight: 400, color: '#666', paddingRight: 50, display: 'flex', flexWrap: 'wrap', alignItems: 'center', marginTop: 8, }}>
                 <p>{description}</p>
               </div> */}
             </div>
             <div style={{ display: 'flex' }}>
-              <img src="https://pbs.twimg.com/profile_images/869985002385231876/q2gatbqh_400x400.jpg" width={172} height={172} style={{}} /> {/* XXX logo image to host ourself on designers*/}
+              <img src="https://pbs.twimg.com/profile_images/869985002385231876/q2gatbqh_400x400.jpg" width={180} height={180} style={{}} /> {/* XXX logo image to host ourself on designers*/}
             </div>
           </div>
-          <div style={{ display: 'flex', height: '640px', backgroundColor: '#06c' }}>
-            {image ? <img style={{
+          {image ? <div style={{ display: 'flex', backgroundColor: '#06c'}}>
+            <img style={{
               width: '100%',
               background: '#06c',
-            }} src={image} /> : ''
-            }
+            }} src={image} />
+          </div> : <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-end', alignContent: 'flex-end' }}>
+            <div style={{ display: 'flex', height: 32, width: '100%', backgroundColor: '#06c', }}>
+              &nbsp;
+            </div>
           </div>
+          }
         </div>
       </div >
     )
