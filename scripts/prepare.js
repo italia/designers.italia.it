6a908b8002016588e495cd1d7dc7585dc662cfdf
Andrea Stagi <stagi.andrea@gmail.com>
2024-07-08 15:36:16 +0200

    fix: update examples generator

--- scripts/prepare.js
+++ scripts/prepare.js
@@ -1,16 +1,16 @@
 /* eslint-disable no-console */ // console.log is ok here for progress reporting
 
-// const { generateExamples } = require("./generate-examples.js");
-// const { downloadExamples } = require("./download-examples.js");
+const { generateExamples } = require("./generate-examples.js");
+const { downloadExamples } = require("./download-examples.js");
 const { prepareComponentsStatus } = require("./prepare-components-status.js");
 
 const main = async () => {
   console.log("⤵️ Downloading examples...");
-  // await downloadExamples("bsi");
-  // console.log("✅ Done");
-  // console.log("🧵 Generating examples...");
-  // generateExamples("bsi");
-  // console.log("✅ Done");
+  await downloadExamples("bsi");
+  console.log("✅ Done");
+  console.log("🧵 Generating examples...");
+  generateExamples("bsi");
+  console.log("✅ Done");
   console.log("📊 Generating components status...");
   await prepareComponentsStatus();
   console.log("✅ Done");
