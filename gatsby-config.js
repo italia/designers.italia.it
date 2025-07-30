04e76b1a740283d5b48c757f590569ff2f20708c
Daniele T <Fupete@users.noreply.github.com>
2025-07-30 11:10:54 +0200

    fix: typo

--- gatsby-config.js
+++ gatsby-config.js
@@ -67,231 +67,231 @@ const GATSBY_PLUGINS = [
       name: `staticImages`,
       path: `${__dirname}/static/images/`,
     },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `editorialBoard`,
-        path: `./src/data/editorial-board`,
-      },
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `content`,
-        path: `./src/data/content`,
-      },
+  },
+  {
+    resolve: `gatsby-source-filesystem`,
+    options: {
+      name: `editorialBoard`,
+      path: `./src/data/editorial-board`,
     },
   },
   {
     resolve: `gatsby-source-filesystem`,
     options: {
-      path: `./src/pages/`,
+      name: `content`,
+      path: `./src/data/content`,
     },
   },
-  {
-    resolve: `@colliercz/gatsby-transformer-gitinfo`,
+{
+  resolve: `gatsby-source-filesystem`,
     options: {
-      include: /\.ya?ml/i,
+    path: `./src/pages/`,
+    },
+},
+{
+  resolve: `@colliercz/gatsby-transformer-gitinfo`,
+    options: {
+    include: /\.ya?ml/i,
       match: {
-        regex: "(last-update-skip)",
+      regex: "(last-update-skip)",
         invert: true,
       },
-    },
   },
-  {
-    resolve: `gatsby-plugin-satorare`,
+},
+{
+  resolve: `gatsby-plugin-satorare`,
     options: {
-      path: `${__dirname}/src/components/social-card-image/SocialCardImage.tsx`,
+    path: `${__dirname}/src/components/social-card-image/SocialCardImage.tsx`,
       width: 1200,
-      height: 630,
-      fonts: [
-        {
-          name: `tRegular`,
-          path: `${__dirname}/static/dist/fonts/Titillium_Web/titillium-web-v10-latin-ext_latin-regular.ttf`,
-        },
-        {
-          name: `tBold`,
-          path: `${__dirname}/static/dist/fonts/Titillium_Web/titillium-web-v10-latin-ext_latin-700.ttf`,
-        },
-      ],
-      target_nodes: ["Content"],
+        height: 630,
+          fonts: [
+            {
+              name: `tRegular`,
+              path: `${__dirname}/static/dist/fonts/Titillium_Web/titillium-web-v10-latin-ext_latin-regular.ttf`,
+            },
+            {
+              name: `tBold`,
+              path: `${__dirname}/static/dist/fonts/Titillium_Web/titillium-web-v10-latin-ext_latin-700.ttf`,
+            },
+          ],
+            target_nodes: ["Content"],
     },
-  },
-  {
-    resolve: "gatsby-plugin-matomo",
+},
+{
+  resolve: "gatsby-plugin-matomo",
     options: {
-      siteId: process.env.MATOMO_SITE_ID,
+    siteId: process.env.MATOMO_SITE_ID,
       matomoUrl: "https://ingestion.webanalytics.italia.it/",
-      siteUrl: process.env.MATOMO_SITE_URL,
-      matomoPhpScript: "matomo.php",
-      matomoJsScript: "matomo.js",
-      enableJSErrorTracking: true,
+        siteUrl: process.env.MATOMO_SITE_URL,
+          matomoPhpScript: "matomo.php",
+            matomoJsScript: "matomo.js",
+              enableJSErrorTracking: true,
     },
-  },
-  {
-    resolve: "gatsby-plugin-local-search",
+},
+{
+  resolve: "gatsby-plugin-local-search",
     options: {
-      // A unique name for the search index. This should be descriptive of
-      // what the index contains. This is required.
-      name: "pages",
+    // A unique name for the search index. This should be descriptive of
+    // what the index contains. This is required.
+    name: "pages",
 
       // Set the search engine to create the index. This is required.
       // The following engines are supported: flexsearch, lunr
       engine: "flexsearch",
 
-      // Provide options to the engine. This is optional and only recommended
-      // for advanced users.
-      //
-      // Note: Only the flexsearch engine supports options.
-      engineOptions: {
-        profile: "score",
+        // Provide options to the engine. This is optional and only recommended
+        // for advanced users.
+        //
+        // Note: Only the flexsearch engine supports options.
+        engineOptions: {
+      profile: "score",
         charset: "latin:advanced",
-        // encode: "extra",
-        tokenize: "strict", // < strict = full word (better performance) | forward or reverse partials...
-        threshold: 11,
-        resolution: 22,
-        depth: 2,
-        filter: [
-          // XXX we need a stemmer also...
-          // array stopwords italian > https://www.ranks.nl/stopwords/italian
-          "a",
-          "adesso",
-          "ai",
-          "al",
-          "alla",
-          "allo",
-          "allora",
-          "altre",
-          "altri",
-          "altro",
-          "anche",
-          "ancora",
-          "avere",
-          "aveva",
-          "avevano",
-          "ben",
-          "buono",
-          "che",
-          "chi",
-          "comprare",
-          "con",
-          "consecutivi",
-          "consecutivo",
-          "cosa",
-          "cui",
-          "da",
-          "del",
-          "della",
-          "dello",
-          "dentro",
-          "deve",
-          "devo",
-          "di",
-          "doppio",
-          "due",
-          "e",
-          "ecco",
-          "fare",
-          "fine",
-          "fino",
-          "fra",
-          "gente",
-          "giu",
-          "ha",
-          "hai",
-          "hanno",
-          "ho",
-          "il",
-          "indietro",
-          "invece",
-          "io",
-          "la",
-          "le",
-          "lei",
-          "lo",
-          "loro",
-          "lui",
-          "lungo",
-          "ma",
-          "me",
-          "meglio",
-          "molta",
-          "molti",
-          "molto",
-          "nei",
-          "nella",
-          "no",
-          "noi",
-          "nome",
-          "nostro",
-          "nove",
-          "nuovi",
-          "nuovo",
-          "o",
-          "oltre",
-          "ora",
-          "otto",
-          "peggio",
-          "pero",
-          "persone",
-          "piu",
-          "poco",
-          "primo",
-          "promesso",
-          "qua",
-          "quarto",
-          "quasi",
-          "quattro",
-          "quello",
-          "questo",
-          "qui",
-          "quindi",
-          "quinto",
-          "rispetto",
-          "sara",
-          "secondo",
-          "sei",
-          "sembra",
-          "sembrava",
-          "senza",
-          "sette",
-          "sia",
-          "siamo",
-          "siete",
-          "solo",
-          "sono",
-          "sopra",
-          "soprattutto",
-          "sotto",
-          "stati",
-          "stato",
-          "stesso",
-          "su",
-          "subito",
-          "sul",
-          "sulla",
-          "tanto",
-          "te",
-          "tempo",
-          "terzo",
-          "tra",
-          "tre",
-          "triplo",
-          "ultimo",
-          "un",
-          "una",
-          "uno",
-          "va",
-          "vai",
-          "voi",
-          "volte",
-          "vostro",
-        ],
+          // encode: "extra",
+          tokenize: "strict", // < strict = full word (better performance) | forward or reverse partials...
+            threshold: 11,
+              resolution: 22,
+                depth: 2,
+                  filter: [
+                    // XXX we need a stemmer also...
+                    // array stopwords italian > https://www.ranks.nl/stopwords/italian
+                    "a",
+                    "adesso",
+                    "ai",
+                    "al",
+                    "alla",
+                    "allo",
+                    "allora",
+                    "altre",
+                    "altri",
+                    "altro",
+                    "anche",
+                    "ancora",
+                    "avere",
+                    "aveva",
+                    "avevano",
+                    "ben",
+                    "buono",
+                    "che",
+                    "chi",
+                    "comprare",
+                    "con",
+                    "consecutivi",
+                    "consecutivo",
+                    "cosa",
+                    "cui",
+                    "da",
+                    "del",
+                    "della",
+                    "dello",
+                    "dentro",
+                    "deve",
+                    "devo",
+                    "di",
+                    "doppio",
+                    "due",
+                    "e",
+                    "ecco",
+                    "fare",
+                    "fine",
+                    "fino",
+                    "fra",
+                    "gente",
+                    "giu",
+                    "ha",
+                    "hai",
+                    "hanno",
+                    "ho",
+                    "il",
+                    "indietro",
+                    "invece",
+                    "io",
+                    "la",
+                    "le",
+                    "lei",
+                    "lo",
+                    "loro",
+                    "lui",
+                    "lungo",
+                    "ma",
+                    "me",
+                    "meglio",
+                    "molta",
+                    "molti",
+                    "molto",
+                    "nei",
+                    "nella",
+                    "no",
+                    "noi",
+                    "nome",
+                    "nostro",
+                    "nove",
+                    "nuovi",
+                    "nuovo",
+                    "o",
+                    "oltre",
+                    "ora",
+                    "otto",
+                    "peggio",
+                    "pero",
+                    "persone",
+                    "piu",
+                    "poco",
+                    "primo",
+                    "promesso",
+                    "qua",
+                    "quarto",
+                    "quasi",
+                    "quattro",
+                    "quello",
+                    "questo",
+                    "qui",
+                    "quindi",
+                    "quinto",
+                    "rispetto",
+                    "sara",
+                    "secondo",
+                    "sei",
+                    "sembra",
+                    "sembrava",
+                    "senza",
+                    "sette",
+                    "sia",
+                    "siamo",
+                    "siete",
+                    "solo",
+                    "sono",
+                    "sopra",
+                    "soprattutto",
+                    "sotto",
+                    "stati",
+                    "stato",
+                    "stesso",
+                    "su",
+                    "subito",
+                    "sul",
+                    "sulla",
+                    "tanto",
+                    "te",
+                    "tempo",
+                    "terzo",
+                    "tra",
+                    "tre",
+                    "triplo",
+                    "ultimo",
+                    "un",
+                    "una",
+                    "uno",
+                    "va",
+                    "vai",
+                    "voi",
+                    "volte",
+                    "vostro",
+                  ],
       },
 
-      // GraphQL query used to fetch all data for the search index. This is
-      // required.
-      query: `
+    // GraphQL query used to fetch all data for the search index. This is
+    // required.
+    query: `
       {
         allContent {
           edges {
@@ -350,73 +350,73 @@ const GATSBY_PLUGINS = [
       // Field used as the reference value for each document.
       ref: "id",
 
-      // List of keys to index. The values of the keys are taken from the
-      // normalizer function below.
-      index: ["title", "description", "body"],
+        // List of keys to index. The values of the keys are taken from the
+        // normalizer function below.
+        index: ["title", "description", "body"],
 
-      // List of keys to store and make available in your UI. The values of
-      // the keys are taken from the normalizer function below.
-      // Default: all fields
-      store: [
-        "id",
-        "template",
-        "relativePath",
-        "path",
-        "title",
-        "description",
-        "tag",
-        "tags",
-      ],
+          // List of keys to store and make available in your UI. The values of
+          // the keys are taken from the normalizer function below.
+          // Default: all fields
+          store: [
+            "id",
+            "template",
+            "relativePath",
+            "path",
+            "title",
+            "description",
+            "tag",
+            "tags",
+          ],
 
-      // Function used to map the result from the GraphQL query. This should
-      // return an array of items to index in the form of flat objects
-      // containing properties to index. The objects must contain the `ref`
-      // field above (default: 'id'). This is required.
-      normalizer: ({ data }) =>
-        data.allContent.edges.map((edge) => ({
-          id: edge.node.id,
-          template: edge.node.metadata?.template?.name,
-          relativePath: `/${edge.node.relativePath}`,
-          tags: edge.node.components?.hero?.kangaroo?.tags,
-          path: `${edge.node.seo?.pathname}`,
-          title: `${edge.node.components?.hero?.title}`,
-          description: `${edge.node.seo?.description}`,
-          tag: `${edge.node.components?.hero?.tag?.label}`,
-          body:
-            `${edge.node.components?.hero?.subtitle} ${edge.node.components?.hero?.text}` +
-            ` ${edge.node.components?.sectionIntro?.title} ${edge.node.components?.sectionIntro?.text} ${edge.node.components?.sectionIntro?.moreText}` +
-            ` ${edge.node.components?.sectionsEditorial
-              ?.map((s) => s.title)
-              .join(" ")} ${edge.node.components?.sectionsEditorial
-              ?.map((s) => s.components?.map((c) => c.title))
-              .join(" ")} ${edge.node.components?.sectionsEditorial2
-              ?.map((s) => s.components?.map((c) => c.title))
-              .join(" ")} ${edge.node.components?.sectionsEditorial
-              ?.map((s) => s.text)
-              .join(" ")} ${edge.node.components?.sectionsEditorial
-              ?.map((s) => s.components?.map((c) => c.text))
-              .join(" ")} ${edge.node.components?.sectionsEditorial2
-              ?.map((s) => s.components?.map((c) => c.text))
-              .join(" ")}`,
-        })),
+            // Function used to map the result from the GraphQL query. This should
+            // return an array of items to index in the form of flat objects
+            // containing properties to index. The objects must contain the `ref`
+            // field above (default: 'id'). This is required.
+            normalizer: ({ data }) =>
+              data.allContent.edges.map((edge) => ({
+                id: edge.node.id,
+                template: edge.node.metadata?.template?.name,
+                relativePath: `/${edge.node.relativePath}`,
+                tags: edge.node.components?.hero?.kangaroo?.tags,
+                path: `${edge.node.seo?.pathname}`,
+                title: `${edge.node.components?.hero?.title}`,
+                description: `${edge.node.seo?.description}`,
+                tag: `${edge.node.components?.hero?.tag?.label}`,
+                body:
+                  `${edge.node.components?.hero?.subtitle} ${edge.node.components?.hero?.text}` +
+                  ` ${edge.node.components?.sectionIntro?.title} ${edge.node.components?.sectionIntro?.text} ${edge.node.components?.sectionIntro?.moreText}` +
+                  ` ${edge.node.components?.sectionsEditorial
+                    ?.map((s) => s.title)
+                    .join(" ")} ${edge.node.components?.sectionsEditorial
+                      ?.map((s) => s.components?.map((c) => c.title))
+                      .join(" ")} ${edge.node.components?.sectionsEditorial2
+                        ?.map((s) => s.components?.map((c) => c.title))
+                        .join(" ")} ${edge.node.components?.sectionsEditorial
+                          ?.map((s) => s.text)
+                          .join(" ")} ${edge.node.components?.sectionsEditorial
+                            ?.map((s) => s.components?.map((c) => c.text))
+                            .join(" ")} ${edge.node.components?.sectionsEditorial2
+                              ?.map((s) => s.components?.map((c) => c.text))
+                              .join(" ")}`,
+              })),
     },
-  },
-  {
-    resolve: "gatsby-plugin-eslint",
+},
+{
+  resolve: "gatsby-plugin-eslint",
     options: {
-      // Gatsby required rules directory
-      rulePaths: [gatsbyRequiredRules],
+    // Gatsby required rules directory
+    rulePaths: [gatsbyRequiredRules],
       // re-add "develop" to the stages once https://github.com/italia/designers.italia.it/issues/848
       // is fixed
       stages: [""],
-      extensions: ["js", "jsx", "ts", "tsx"],
-      exclude: ["node_modules", "bower_components", ".cache", "public"],
+        extensions: ["js", "jsx", "ts", "tsx"],
+          exclude: ["node_modules", "bower_components", ".cache", "public"],
       // Any additional eslint-webpack-plugin options below
       // ...
     },
-  },
-  // This must be the last plugin in the array
-  `gatsby-plugin-meta-redirect`,
+},
+// This must be the last plugin in the array
+`gatsby-plugin-meta-redirect`,
 ];
 
 /* Dev / Production configurations and plugins */
