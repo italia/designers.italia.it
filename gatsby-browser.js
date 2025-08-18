ec440c5c8d919743e09a52733c02688d7d080416
Daniele T. <Fupete@users.noreply.github.com>
2025-08-18 22:47:08 +0200

    fix: uodate gatsby-browser scroll handler

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,7 +1,19 @@
-exports.shouldUpdateScroll = ({ routerProps: { location } }) => {
-  if (location.hash === "") {
-    window.scrollTo(0, 0);
-    document.scrollingElement.scrollTop = 0;
+exports.shouldUpdateScroll = ({ 
+  routerProps: { location }, 
+  getSavedScrollPosition 
+}) => {
+  // If there's a hash, let Gatsby handle it (scroll to element)
+  if (location.hash) {
+    return true;
   }
+  // For back/forward navigation, restore saved scroll position
+  const savedPosition = getSavedScrollPosition(location);
+  if (savedPosition) {
+    window.scrollTo(...savedPosition);
+    return false;
+  }
+  // For new navigation (menu clicks, direct links), scroll to top
+  window.scrollTo(0, 0);
+  document.scrollingElement.scrollTop = 0;
   return false;
 };
