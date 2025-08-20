9a6d3aa1eceb37c90c094df6dbffbff933714a02
Daniele T <Fupete@users.noreply.github.com>
2025-08-20 11:05:02 +0200

    fix: react import globally for old third parties libraries and fix eslintrc for new React 18

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,4 +1,12 @@
-exports.shouldUpdateScroll = ({ routerProps: { location } }) => {
+// gatsby-browser.js
+import React from 'react';
+
+// Make React available globally for older third-party libraries
+if (typeof window !== 'undefined') {
+  window.React = React;
+}
+
+export const shouldUpdateScroll = ({ routerProps: { location } }) => {
   if (location.hash === "") {
     window.scrollTo(0, 0);
     document.scrollingElement.scrollTop = 0;
