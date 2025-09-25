7342cc2c83008ed34eae4bad7c343da85d61493d
Francesco Improta <me@francescoimprota.com>
2025-09-25 16:12:05 +0200

    fix: linter errors

--- src/components/tag/tag.js
+++ src/components/tag/tag.js
@@ -7,11 +7,15 @@ function Tag({ url, children, label, addonClasses, screenReaderText }) {
   const styles = classNames("di-tag", addonClasses); // don't use generic ".tag" > conflicts with Prism syntax highlighter
 
   const Container = url ? Link : "div";
-  const containerProps = url ? { to: url, className: styles } : { className: styles };
+  const containerProps = url
+    ? { to: url, className: styles }
+    : { className: styles };
 
   return (
     <Container {...containerProps}>
-      {screenReaderText && <span className="visually-hidden">{screenReaderText}</span>}
+      {screenReaderText && (
+        <span className="visually-hidden">{screenReaderText}</span>
+      )}
       <span>{label || children}</span>
     </Container>
   );
