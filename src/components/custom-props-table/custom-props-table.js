ed54e65d3171bef243ab195a9be5b5455f3df175
Andrea Stagi <stagi.andrea@gmail.com>
2026-08-27 10:31:46 +0200

    chore: try to build with imported json

--- src/components/custom-props-table/custom-props-table.js
+++ src/components/custom-props-table/custom-props-table.js
@@ -1,16 +1,16 @@
 import classNames from "classnames";
 import React from "react";
 import "./custom-props-table.scss";
-
+import customProperties from "../../data/components_json/bsi/custom_properties.json";
 
 function CustomPropsTable({
   title,
 }) {
   // heading level
-
+  console.log(customProperties);
   return (
     <div className="custom-props-table">
-      {title && <h2 className="mb-3">{title}</h2>}
+      {title && <h2 className="mb-3">{customProperties.acceptoverlay[0].description}</h2>}
     </div>
   );
 }
